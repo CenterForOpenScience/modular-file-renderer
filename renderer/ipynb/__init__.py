7e478e2342f9c8d246139979074c553b33bcecef
Nezar Abdennur <nabdennur@gmail.com>
2013-10-20 04:19:09 -0400

    added exporters for ipynb-json and py files

--- renderer/ipynb/__init__.py
+++ renderer/ipynb/__init__.py
@@ -1,17 +1,17 @@
 from .. import FileRenderer
 from flask import render_template
 
-from IPython.nbformat import current as nbformat
-from IPython.nbconvert.exporters import HTMLExporter
 from IPython.config import Config
+from IPython.nbconvert import export_python
+from IPython.nbconvert.exporters import HTMLExporter
+from IPython.nbformat import current as nbformat
 
-# Instantiate and configure the exporter
-config = Config()
-config.HTMLExporter.template_file = 'basic'
-config.NbconvertApp.fileext = 'html'
-config.CSSHTMLHeaderTransformer.enabled = False
-config.Exporter.filters = {'strip_files_prefix': lambda s: s} #don't strip the files prefix
-html_exporter = HTMLExporter(config=config)
+c = Config()
+c.HTMLExporter.template_file = 'basic'
+c.NbconvertApp.fileext = 'html'
+c.CSSHTMLHeaderTransformer.enabled = False
+c.Exporter.filters = {'strip_files_prefix': lambda s: s} #don't strip the files prefix
+exporter = HTMLExporter(config=c)
 
 class NbFormatError(Exception):
     pass
@@ -27,13 +27,20 @@ class IPynbRenderer(FileRenderer):
     def render(self, fp, path):
         content = fp.read()
         nb = self._parse_json(content)
-        name, css_theme = self._get_metadata(nb)
-        body = html_exporter.from_notebook_node(nb)[0] 
+        name, theme = self._get_metadata(nb)
+        body = exporter.from_notebook_node(nb)[0] 
         context =  {'file_name': name,
-                    'css_theme': css_theme,
+                    'css_theme': theme, #'css_linalg', 'cdp_1'
                     'mathjax_conf': None,
                     'body': body }
         return render_template('notebook.html', **context)
+
+    def export_ipynb(self, fp):
+        return fp.read(), '.ipynb'
+
+    def export_python(self, fp):
+        output, resources = export_python(fp)
+        return output, '.py'
 
     def _parse_json(self, content):
         try :
@@ -47,7 +54,7 @@ class IPynbRenderer(FileRenderer):
         name = nb.get('metadata', {})\
                  .get('name', None) 
         if not name:
-            name = url.rsplit('/')[-1]
+            name = "untitled.ipynb"
         if not name.endswith(".ipynb"):
             name = name + ".ipynb"
 

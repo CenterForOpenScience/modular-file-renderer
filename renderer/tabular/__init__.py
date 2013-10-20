a7a340218241fe60054784f1e60ae51272803590
Melissa Lewis <meli.lewis@gmail.com>
2013-10-20 09:06:51 -0400

    more js and css, relevant html examples

--- renderer/tabular/__init__.py
+++ renderer/tabular/__init__.py
@@ -1,5 +1,5 @@
 from .. import FileRenderer
-import pandas
+import pandas as pd
 import json
 
 class TabularRenderer(FileRenderer):
@@ -8,7 +8,25 @@ class TabularRenderer(FileRenderer):
         return fp.name.endswith('csv')
 
     def render(self, fp, path):
-        return '''<table width="100%">
+        return '''
+        <script src="../lib/jquery-1.7.min.js"></script>
+        <script src="../lib/jquery.event.drag-2.2.js"></script>
+        <script src="../slick.core.js"></script>
+        <script src="../slick.grid.js"></script>
+        <script>
+            var grid;
+var columns = [
+    {   'field': 'employees', 'id': 'employees', 'name': 'employees'},
+    {   'field': 'gender', 'id': 'gender', 'name': 'gender'},
+    {   'field': 'desk', 'id': 'desk', 'name': 'desk'}];
+
+  var options = {
+    enableCellNavigation: true,
+    enableColumnReorder: false
+  };
+
+  $(function () {
+        <table width="100%">
   <tr>
     <td valign="top" width="50%">
       <div id="myGrid" style="width:600px;height:500px;"></div>

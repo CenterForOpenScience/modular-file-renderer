5c89e1fc7caa73aa7ae30e761830ffd8006a66d6
Kurtis Jungersen <kmjungersen@gmail.com>
2014-04-03 14:36:08 -0400

    Random example changes that weren't previously added

---
+++ renderer/ipynb/fixtures/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''This is a test Python file!'''
+
+x = 2
+y = 4
+
+print "Hello world!' Let's do math!"
+print "x + y = ", x + y
+print "x * y = ", x * y
+print "Yay!"
+
+print "Lookie, we even support umlauts!    -----> ü <-----"

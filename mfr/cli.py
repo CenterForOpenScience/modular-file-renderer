f8dfd2a5edb0146af77ccc7603a1e235867676ff
Steven Loria <sloria1@gmail.com>
2014-04-06 14:12:58 -0400

    Add <module> argument

--- mfr/cli.py
+++ mfr/cli.py
@@ -1,8 +1,8 @@
 """mfr: CLI for installing mfr modules.
 
 Usage:
-    mfr init
-    mfr install [--exclude-static]
+    mfr init [-d]
+    mfr install <module>... [--exclude-static] [-d]
 
 Options:
     -h --help       Show this screen.
@@ -10,21 +10,31 @@ Options:
     install         Reads mfrconfig file and installs all necessary dependencies.
 
 """
+import os
+import os.path.join as pjoin
 import logging
 
+import pip
 from docopt import docopt
 
-logger = logging.getLogger(__name__)
+import mfr
 
+logger = logging.getLogger(__name__)
+debug = logger.debug
 
 def parse_args():
     args = docopt(__doc__)
     return args
 
-
-def install_module():
+def find_requirements_files(dir):
     # TODO(sloria)
     pass
+
+def install_module(module_name):
+    # TODO(sloria)
+    debug('Installing {0!r}'.format(module_name))
+    module_dir = pjoin(mfr.PACKAGE_DIR, module_name)
+    req_files = find_requirements_files(module_dir)
 
 
 def main():
@@ -35,7 +45,11 @@ def main():
             format='%(levelname)s %(filename)s: %(message)s',
             level=logging.DEBUG)
     logger.debug(args)
-    module = args['<module>']
+    modules_to_install = args['<module>']
+    for module_name in modules_to_install:
+        install_module(module_name)
+
+
 
 if __name__ == '__main__':
     main()

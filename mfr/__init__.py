cbcac5000f58c48510cedb829d01a32a1460d49a
Steven Loria <sloria1@gmail.com>
2014-04-06 13:54:36 -0400

    Add mfr.PACKAGE_DIR constant

--- mfr/__init__.py
+++ mfr/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 
 __version__ = '0.1.0-dev'
 __author__ = 'Center for Open Science'
 
 
 from mfr.core import render, detect, FileHandler, get_file_extension, register_filehandler, export, get_file_exporters
+
+
+PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))

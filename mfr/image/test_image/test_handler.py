d02003b61a1acaf1c313fd64a8b5e354491e109f
Steven Loria <sloria1@gmail.com>
2014-04-07 11:10:13 -0400

    Catch ImportError in image/handler.py.
    Allows ImageFileHandler to be used without PIL

--- mfr/image/test_image/test_handler.py
+++ mfr/image/test_image/test_handler.py
@@ -1,7 +1,6 @@
 from mfr.image.handler import ImageFileHandler
 
 
-# TODO: Move this to test_handler.py?
 def test_image_handler_detect_image(fakefile):
     # set the file's name
     fakefile.name = 'myimage.jpg'

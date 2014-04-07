8dbcf007428f5e79cd088dc154e3e64184b9c63d
Steven Loria <sloria1@gmail.com>
2014-04-07 11:45:57 -0400

    That doesn't belong there

--- mfr/docx/handler.py
+++ mfr/docx/handler.py
@@ -3,11 +3,8 @@
 from mfr.core import FileHandler, get_file_extension
 from mfr.docx.render import render_html
 
-from mfr.docx.export import DocxExporter
-
 EXTENSIONS = [
     '.docx',
-    # TODO: finish this list
 ]
 
 
@@ -16,13 +13,6 @@ class DocxFileHandler(FileHandler):
     renderers = {
         # like functions
         'html': render_html,
-    }
-
-    exporters = {
-        # Or instance methods
-        'pdf': DocxExporter().export_pdf,
-        'txt': DocxExporter().export_txt,
-        # ...
     }
 
     def detect(self, fp):

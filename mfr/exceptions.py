9d5d8b6994dcc2b5970fc851e1311319071c562c
Steven Loria <sloria1@gmail.com>
2014-04-08 13:14:39 -0400

    Add configuration error

--- mfr/exceptions.py
+++ mfr/exceptions.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """Exception classes for the mfr package."""
 
+
 class MFRException(Exception):
     """Base exception from which all MFR-related errors inherit."""
+    pass
+
+
+class ConfigurationError(MFRException):
+    """Error raised when MFR is improperly configured."""
     pass

76f02d5d0c6a764d25819f007429dde2ddd8c84d
Alexander Schiller <alexschiller@gmail.com>
2014-04-08 14:46:20 -0400

    Test changes and export requirements
    - Add code detect and not detect test
    - Update image detect and add image not detect test
    - Update render/export requirements for code/image

--- mfr/code/test_code.py
+++ mfr/code/test_code.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import pytest
-
+import mfr
 from mfr.code.handler import CodeFileHandler
-
-
+from mfr.code.render import render_html
+mfr.register_filehandler("code", CodeFileHandler)
 @pytest.mark.parametrize('filename', [
     'script.py',
     'script.rb',
@@ -16,3 +16,17 @@ def test_detect_common_extensions(fakefile, filename):
     fakefile.name = filename
     handler = CodeFileHandler()
     assert handler.detect(fakefile) is True
+
+@pytest.mark.parametrize('filename', [
+    'other.y',
+    'otherpy',
+    'other.bump',
+    'other.',
+])
+def test_does_not_detect_other_extensions(fakefile, filename):
+    fakefile.name = filename
+    handler = CodeFileHandler()
+    assert handler.detect(fakefile) is False
+
+
+

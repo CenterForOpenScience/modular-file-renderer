9dbb73b204f62c1c8cd6d57059c3299783f46326
Alexander Schiller <alexschiller@gmail.com>
2014-04-08 16:47:27 -0400

    Clean module template and removed lingering static folders

--- mfr/template/render.py
+++ mfr/template/render.py
@@ -1,1 +1,10 @@
 """TEMPLATE renderer module."""
+
+
+def render_html(fp, *args, **kwargs):
+    """A simple TEMPLATE renderer. Takes file pointer and returns html
+
+    :param str:
+    """
+    html = fp.read()
+    return html

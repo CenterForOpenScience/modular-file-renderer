da975209d4d884d4188d0cf099eb8fb5ea1e9e87
Tom Baxter <tkb608@gmail.com>
2014-04-14 15:02:27 -0400

    Improved tests for mfr_md
    Added test for __init__.py for 100% coverage

--- mfr_md/test_md.py
+++ mfr_md/test_md.py
@@ -1,5 +1,5 @@
-import mfr_md
-from mfr_md import render
+from mfr_md import render, Handler
+from mfr.core import FileHandler
 
 def test_render_html(fakefile):
     fakefile.read.return_value = '# foo'
@@ -8,4 +8,20 @@ def test_render_html(fakefile):
     assert render.render_html(fakefile) == '<p><em>italic</em></p>'
     fakefile.read.return_value = '*italic*'
     assert render.render_html(fakefile) == '<p><em>italic</em></p>'
-    
+    fakefile.read.return_value = '''
+* one
+* two'''
+    assert render.render_html(fakefile) == '''<ul>
+<li>one</li>
+<li>two</li>
+</ul>'''
+        
+def test_handler(fakefile):
+    testHandler=Handler()
+    fakefile.name='file.notmd'
+    assert testHandler.detect(fakefile) == False
+    fakefile.name='file.md'
+    assert testHandler.detect(fakefile) == True
+    fakefile.name='file.markdown'
+    assert testHandler.detect(fakefile) == True
+

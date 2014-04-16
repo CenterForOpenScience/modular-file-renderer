b54bfc68e27155a328b3444ace0b1080a1ff8623
Elijah Hamovitz <elijahhamovitz@gmail.com>
2014-04-16 08:06:15 -0700

    restructured create_app so that it can be called from project root. Also realized that the MFR configuration values don't actually have to be embedded in before_request

--- player/app/__init__.py
+++ player/app/__init__.py
@@ -10,47 +10,48 @@
 """
 from flask import Flask, render_template
 
-# TODO the mfr import and configuration system could use a whole ton of
-# work. Highest priority is putting that pygments theme config option
-# into the main config object with everything else. Second priority is
-# coming up with a much more dynamic way to configure the MFR based on
-# this app
 import mfr
 import mfr_image
 import mfr_docx
 import mfr_rst
 import mfr_md
 import mfr_code_pygments
+
+# TODO this is rather gross; the MFR Code Pygments module really neesd a
+# much better configuration system
 from mfr_code_pygments.configuration import config as mfr_code_config
 mfr_code_config['PYGMENTS_THEME'] = 'manni'
 
 
-def create_app(config_overrides={}):
+def create_app(**kwargs):
     """Create and return an Flask app instance"""
+
     # create app; load config
+
     app = Flask(__name__)
-    app.config.from_object('config')
-    app.config.update(**config_overrides)
+    app.config.from_object('player.config')
+    app.config.update(**kwargs)
 
-    @app.before_request
-    def before_request():
-        # TODO as referenced above, this needs to be fixed
-        class MFRConfig:
-            # Base URL for static files
-            STATIC_URL = app.static_url_path
-            # Where to save static files
-            STATIC_FOLDER = app.static_folder
-            # Allow renderers to include static asset imports
-            INCLUDE_STATIC = True
+    # Configure MFR
 
-            # Available file handlers
-            HANDLERS = [mfr_image.Handler,
-                        mfr_docx.Handler,
-                        mfr_rst.Handler,
-                        mfr_md.Handler,
-                        mfr_code_pygments.Handler]
-        mfr.config.from_object(MFRConfig)
-        mfr.collect_static()
+    class MFRConfig:
+        # Base URL for static files
+        STATIC_URL = app.static_url_path
+        # Where to save static files
+        STATIC_FOLDER = app.static_folder
+        # Allow renderers to include static asset imports
+        INCLUDE_STATIC = True
+
+        # Available file handlers
+        HANDLERS = [mfr_image.Handler,
+                    mfr_docx.Handler,
+                    mfr_rst.Handler,
+                    mfr_md.Handler,
+                    mfr_code_pygments.Handler]
+    mfr.config.from_object(MFRConfig)
+    mfr.collect_static()
+
+    # Set up error handlers
 
     @app.errorhandler(404)
     def not_found_error(error):
@@ -65,10 +66,11 @@ def create_app(config_overrides={}):
         return render_template('501.html'), 501
 
     # register blueprints
-    from app.main.views import mod as main_module
+
+    from player.app.main.views import mod as main_module
     app.register_blueprint(main_module)
 
-    from app.render.views import mod as render_module
+    from player.app.render.views import mod as render_module
     app.register_blueprint(render_module)
 
     return app

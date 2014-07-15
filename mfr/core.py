de9bec987926b5acb544fbaac8db8f6f9e2a600d
Austin <asmacdo@gmail.com>
2014-07-15 13:45:51 -0400

    add default FileHandler name

--- mfr/core.py
+++ mfr/core.py
@@ -184,6 +184,7 @@ class FileHandler(object):
     renderers = {}
     #: Maps exporter names to exporter callables, e.g. {'png': export_png}
     exporters = {}
+    name = "Unnamed handler"
 
     default_renderer = 'html'
     default_exporter = None

d34f228e4f215626f28b200f0413a1ee283f02f6
Austin <asmacdo@gmail.com>
2014-07-20 16:29:12 -0400

    clean import

--- mfr_docx/render.py
+++ mfr_docx/render.py
@@ -1,7 +1,7 @@
 """Docx renderer module."""
 
 import pydocx
-from mfr.core import RenderResult
+from mfr import RenderResult
 
 def render_docx(fp, *args, **kwargs):
     return RenderResult(pydocx.Docx2Html(fp).parsed)

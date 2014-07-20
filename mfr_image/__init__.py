0e2d64b5b800210206b17cab8784879b71cb7104
Austin <asmacdo@gmail.com>
2014-07-20 16:26:47 -0400

    recombine render and export for image

--- mfr_image/__init__.py
+++ mfr_image/__init__.py
@@ -2,6 +2,18 @@
 from mfr.core import FileHandler, get_file_extension
 from mfr_image.render import render_img_tag
 
+try:  # Exporter requires PIL
+    from mfr_image.export import ImageExporter
+    exporter = ImageExporter()
+    exporters = {
+        # 'png': exporter.export_png,
+        'jpg': exporter.export_jpg,
+        'gif': exporter.export_gif,
+        # 'tif': exporter.export_tif,
+    }
+except ImportError:
+    exporters = {}
+
 EXTENSIONS = [
     '.jpeg',
     '.jpg',
@@ -17,6 +29,8 @@ class Handler(FileHandler):
     renderers = {
         'html': render_img_tag,
     }
+
+    exporters = exporters
 
     def detect(self, fp):
         return get_file_extension(fp.name) in EXTENSIONS
--- mfr_image/render.py
+++ mfr_image/render.py
@@ -10,5 +10,5 @@ def render_img_tag(fp, src=None, alt=''):
     if src is None:
         src = fp.name
 
-    # return RenderResult('<img src="{src}" alt="{alt}" />'.format(src=src, alt=alt))
-    return ('<img src="{src}" alt="{alt}" />'.format(src=src, alt=alt))
+    return RenderResult('<img src="{src}" alt="{alt}" />'.format(src=src, alt=alt))
+    # return ('<img src="{src}" alt="{alt}" />'.format(src=src, alt=alt))

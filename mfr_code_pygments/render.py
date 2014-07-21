b376a490e0e4930531be51f4e4d0787952ff63ed
Austin <asmacdo@gmail.com>
2014-07-21 08:39:47 -0400

    remove depenency on INCLUDE_STATIC

--- mfr_code_pygments/render.py
+++ mfr_code_pygments/render.py
@@ -4,7 +4,8 @@ import pygments
 import pygments.lexers
 import pygments.formatters
 
-from mfr import config as core_config, RenderResult
+from mfr import config as core_config
+from mfr import RenderResult
 
 from mfr_code_pygments.configuration import config as module_config
 
@@ -19,17 +20,12 @@ def render_to_html(fp, *args, **kwargs):
     content = fp.read()
     lexer = pygments.lexers.guess_lexer_for_filename(fp.name, content)
     content = pygments.highlight(content, lexer, formatter)
-    if core_config['INCLUDE_STATIC']:
-        link = get_stylesheet()
-        # return '\n'.join([link, content])
-        assets = {"css": link}
-        return RenderResult(content=content, assets=assets)
-    else:
-        return content
+    assets = {"css": [get_stylesheet()]}
+    return RenderResult(content=content, assets=assets)
 
 
 def get_stylesheet():
     """Generate an html link to a stylesheet"""
-    
+
     return '<link rel="stylesheet" href="{static_url}/mfr_code_pygments/css/{theme}.css" />'\
         .format(static_url=core_config['STATIC_URL'], theme=module_config['PYGMENTS_THEME'])

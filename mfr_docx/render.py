9a0d9986c474a79847b7f841acedc7c7fde257a5
Austin <asmacdo@gmail.com>
2014-07-22 08:25:47 -0400

    use previous version of pydocx for file pointers

--- mfr_docx/render.py
+++ mfr_docx/render.py
@@ -5,5 +5,5 @@ from mfr import RenderResult
 
 
 def render_docx(fp, *args, **kwargs):
-    content = pydocx.Docx2Html(fp.name).parsed
+    content = pydocx.Docx2Html(fp).parsed
     return RenderResult(content=content, assets={})

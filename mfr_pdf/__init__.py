e0001971685ba378da4360e423203661e3c640de
Oliver M Daniel <olivermdaniel@gmail.com>
2014-07-23 16:19:50 -0400

    Finished init detect, work-in-progress render.py mako refactoring

--- mfr_pdf/__init__.py
+++ mfr_pdf/__init__.py
@@ -3,35 +3,22 @@ from mfr.core import FileHandler, get_file_extension
 from mfr_pdf.render import render_pdf_mako
 import PyPDF2
 
+
+def is_valid(fp)
+    try:
+        PyPDF2.PdfFileReader(fp)
+        return True
+    except PyPDF2.utils.PdfReadError:
+        return False
+
+
 class Handler(FileHandler):
     """The image file handler."""
     renderers = {
-        'html': render_img_tag,
+        'html': render_pdf_mako,
     }
-    exporters = exporters
+
+    exporters = {}
 
     def detect(self, fp):
-        return get_file_extension(fp.name) in EXTENSIONS
-
-class PdfRenderer(FileRenderer):
-
-    # Gets here using the .pdf extension check then attempts to read the file
-        # using pydf2, if it can it accepts it as a valid pdf
-
-    def _detect(self, file_pointer):
-        _, ext = os.path.splitext(file_pointer.name)
-        if ext.lower() == ".pdf":
-            try:
-                PyPDF2.PdfFileReader(file_pointer)
-            except PyPDF2.utils.PdfReadError:
-                return False
-            return True
-        return False
-
-    def _render(self, file_pointer, **kwargs):
-        url = kwargs['url']
-        return self._render_mako(
-            "pdfpage.mako",
-            url=url,
-            STATIC_PATH=self.STATIC_PATH,
-        )
+        return get_file_extension(fp.name) in EXTENSIONS and is_valid(fp)
--- mfr_pdf/render.py
+++ mfr_pdf/render.py
@@ -1,6 +1,7 @@
 """PDF renderer module."""
 from mfr.core import RenderResult
 from mako.lookup import TemplateLookup
+import PyPDF2
 
 def render_pdf_mako(fp, src=None, alt=''):
     """A simple pdf renderer.
@@ -15,3 +16,18 @@ def render_pdf_mako(fp, src=None, alt=''):
     content =
     return RenderResult(content)
 
+mako_lookup = TemplateLookup(
+            directories=['/templates']
+        )
+
+def render_mako(fp, **kwargs):
+    return mako_lookup.get_template(filename).render(**kwargs)
+
+
+def render_pdf_mako(fp, **kwargs):
+    url = kwargs['url']
+    return render_mako(
+        "pdfpage.mako",
+        url=url,
+        STATIC_PATH=self.STATIC_PATH,
+    )

d275fd43b8faca4f36fa10f50dd51fa06de059d7
Oliver M Daniel <olivermdaniel@gmail.com>
2014-07-23 17:10:00 -0400

    render.py ready to test

--- mfr_pdf/render.py
+++ mfr_pdf/render.py
@@ -1,33 +1,19 @@
 """PDF renderer module."""
 from mfr.core import RenderResult
 from mako.lookup import TemplateLookup
+from mfr import config as core_config
 import PyPDF2
 
-def render_pdf_mako(fp, src=None, alt=''):
+template  = TemplateLookup(
+            directories=['/templates']
+        ).get_template("pdfpage.mako")
+
+def render_pdf_mako(fp, url=None):
     """A simple pdf renderer.
 
     :param str:
     """
-    # Default src to the filename
-    if src is None:
-        src = fp.name
+    url = url or fp.name
 
-    # content = '<img src="{src}" alt="{alt}" />'.format(src=src, alt=alt)
-    content =
+    content = template.render(url=url,STATIC_PATH=core_config['STATIC_URL'])
     return RenderResult(content)
-
-mako_lookup = TemplateLookup(
-            directories=['/templates']
-        )
-
-def render_mako(fp, **kwargs):
-    return mako_lookup.get_template(filename).render(**kwargs)
-
-
-def render_pdf_mako(fp, **kwargs):
-    url = kwargs['url']
-    return render_mako(
-        "pdfpage.mako",
-        url=url,
-        STATIC_PATH=self.STATIC_PATH,
-    )

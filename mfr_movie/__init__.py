2cc9a0137662ea737464250369a32d11a85fa40a
Austin Macdonald <asmacdo@gmail.com>
2014-07-29 16:34:32 -0400

    basic video in the browser

---
+++ mfr_movie/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from mfr.core import FileHandler, get_file_extension
+from mfr_movie.render import render_movie_tag
+
+# TODO(asmacdo) add exporter
+exporters = {}
+
+EXTENSIONS = [
+    # TODO(asmacdo) add more
+    '.mp4'
+]
+
+
+class Handler(FileHandler):
+    """The movie file handler."""
+
+    renderers = {
+        'html': render_movie_tag,
+    }
+
+    exporters = exporters
+
+    def detect(self, fp):
+        return get_file_extension(fp.name) in EXTENSIONS

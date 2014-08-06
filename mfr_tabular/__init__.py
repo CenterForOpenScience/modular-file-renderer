a721889d79bdc9dc79111d77ec6b8dd3a0e54e43
Austin Macdonald <asmacdo@gmail.com>
2014-08-06 11:08:23 -0400

    initial commit

---
+++ mfr_tabular/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from mfr.core import FileHandler, get_file_extension
+from mfr_tabular.render import render_html
+
+EXTENSIONS = [
+    '.csv',
+]
+
+
+class Handler(FileHandler):
+    renderers = {
+        'html':  render_html
+    }
+
+    def detect(self, fp):
+        return get_file_extension(fp.name) in EXTENSIONS
---
+++ mfr_tabular/render.py
@@ -0,0 +1,9 @@
+import pandas
+from mfr.core import RenderResult
+
+
+def render_html(fp, src=None):
+
+    return pandas.read_csv(fp.name)
+
+    return RenderResult("Hello")

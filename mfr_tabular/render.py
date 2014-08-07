2b03f84009b51fc721567452cb8f1b9edb0eb035
Austin Macdonald <asmacdo@gmail.com>
2014-08-07 11:38:23 -0400

    working with csv or pandas

--- mfr_tabular/render.py
+++ mfr_tabular/render.py
@@ -1,21 +1,24 @@
-# import pandas
-import csv
 from mfr.core import RenderResult
-# import json
 from mako.lookup import TemplateLookup
 
+try:
+    import pandas
+    PANDAS = True
+except IOError:
+    PANDAS = False
+    import csv
 template = TemplateLookup(
     directories=['mfr_tabular/templates']
 ).get_template('tabular.mako')
 
 
-def column_population(fields):
+def column_population(data):
     """make column headers from the keys in dataframe
     :param dataframe:
     :return: a list of dictionaries
     """
     columns = []
-    for field in fields:
+    for field in data:
         columns.append({
             'id': field,
             'name': field,
@@ -45,36 +48,55 @@ def row_population(data, fields=None):
     return rows
 
 
-def render_html(fp, src=None):
+def pandas_row_pop(dataframe):
+    fields = dataframe.keys()
+    rows = []
+    for n in range(len(dataframe[fields[0]])):
+        rows.append({})
+        for col_fields in fields:
+            rows[n][col_fields] = str(dataframe[col_fields][n])
+
+    return rows
+
+
+def data_from_csv(fp):
 
     with open(fp.name) as infile:
         dialect = csv.Sniffer().sniff(infile.read(1024))
         infile.seek(0)
-        has_header = csv.Sniffer().has_header(infile.read())
-        infile.seek(0)
+        # TODO(asmacdo) Nice way of displaying if there is no header
+        # has_header = csv.Sniffer().has_header(infile.read())
+        # infile.seek(0)
         reader = csv.reader(infile, dialect)
         data = [row for row in reader]
 
-    header = []
-    if has_header:
-        print "yessss"
-        header = data[0]
-        data = data[1:]
+    columns = column_population(data[0])
+    rows = row_population(data[1:], data[0])
 
-    # dataframe = pandas.read_csv(fp.name)
-    print "header", header
-    print "data", data
+    return columns, rows
 
-    columns = column_population(header)
-    rows = row_population(data, fields=header)
 
-    print "columns", columns
-    print "rows", rows
+def data_from_pandas(fp):
+
+    dataframe = pandas.read_csv(fp.name)
+    fields = dataframe.keys()
+    columns = column_population(fields)
+    rows = pandas_row_pop(dataframe)
+
+    return columns, rows
+
+
+def render_html(fp, src=None):
+
+    if PANDAS:
+        columns, rows = data_from_pandas(fp)
+    else:
+        columns, rows = data_from_csv(fp)
 
     content = template.render(
         columns=columns,
         rows=rows,
-        writing='yes',
+        writing="Pandas = " + str(PANDAS),
         STATIC_PATH="/mfr/mfr_tabular",
     )
 

9230a615092152bed4aeea2a378baaea75d19c2b
Austin Macdonald <asmacdo@gmail.com>
2014-08-07 16:18:51 -0400

    remove redundant file open

--- mfr_tabular/csv_tools.py
+++ mfr_tabular/csv_tools.py
@@ -8,14 +8,13 @@ def data_from_csv(fp):
     :return: tuple of columns and rows
     """
 
-    with open(fp.name) as infile:
-        dialect = csv.Sniffer().sniff(infile.read(1024))
-        infile.seek(0)
-        # TODO(asmacdo) Nice way of displaying if there is no header
-        # has_header = csv.Sniffer().has_header(infile.read())
-        # infile.seek(0)
-        reader = csv.reader(infile, dialect)
-        data = [row for row in reader]
+    dialect = csv.Sniffer().sniff((fp).read(1024))
+    fp.seek(0)
+    # TODO(asmacdo) Nice way of displaying if there is no header
+    # has_header = csv.Sniffer().has_header(infile.read())
+    # infile.seek(0)
+    reader = csv.reader(fp, dialect)
+    data = [row for row in reader]
 
     columns = column_population(data[0])
     rows = row_population(data[1:], data[0])

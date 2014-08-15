b4e0e36290078f96135e280e41c2427bd6d406b6
Austin Macdonald <asmacdo@gmail.com>
2014-08-15 10:05:24 -0400

    clean and idiomize

--- mfr_ipynb/tests/test_ipynb.py
+++ mfr_ipynb/tests/test_ipynb.py
@@ -5,13 +5,12 @@ import pytest
 from mfr_ipynb import Handler as CodeFileHandler
 from mfr_ipynb.render import render_html
 
- 
-
 
 def test_detect_correct_extension(fakefile):
     fakefile.name = 'hello.ipynb'
     handler = CodeFileHandler()
     assert handler.detect(fakefile) is True
+
 
 @pytest.mark.parametrize('filename', [
     'other.y',
@@ -23,4 +22,3 @@ def test_detect_other_extensions(fakefile, filename):
     fakefile.name = filename
     handler = CodeFileHandler()
     assert handler.detect(fakefile) is False
-

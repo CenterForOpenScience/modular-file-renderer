a7dd6fbe644ede2fe1967698e4976b94fac36cc7
Oliver M Daniel <olivermdaniel@gmail.com>
2014-08-18 20:01:25 +0000

    Could not get static assets working by themselves, using Mapbox CDN which is recommended usage in Mapbox API documentation

--- mfr_geo/render.py
+++ mfr_geo/render.py
@@ -5,16 +5,6 @@ from jinja2 import Template
 with open('mfr_geo/templates/geo_base_template.html') as fid:
     template = Template(fid.read())
 
-def get_assets():
-    """Creates a dictionary of js and css assets"""
-
-    static_dir = "/static/mfr/mfr_pdf"
-
-    assets = {}
-    assets['js']  = [static_dir + '/js/mapbox.js']
-    assets['css'] = [static_dir + '/css/mapbox.css']
-
-    return assets
 
 def render_geo(fp, src=None):
     """A simple geographic (geo) file renderer.
@@ -23,4 +13,4 @@ def render_geo(fp, src=None):
     """
 
     content = template.render(geo_json = fp.read(), geo_filename = fp.name)
-    return RenderResult(content, assets=get_assets()) 
+    return RenderResult(content) 

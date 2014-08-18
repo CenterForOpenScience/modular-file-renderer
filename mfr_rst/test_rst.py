063b9a3f5b7578afbf5385ea7a817db163a5de10
asmacdo <asmacdo@gmail.com>
2014-08-18 10:58:08 -0400

    Test render returns RenderResult

--- mfr_rst/test_rst.py
+++ mfr_rst/test_rst.py
@@ -1,4 +1,5 @@
-
+import pytest
+import mfr
 import mfr_rst
 
 
@@ -7,3 +8,23 @@ def test_detect(fakefile):
     fakefile.name = 'mydoc.rst'
     handler = mfr_rst.Handler()
     assert handler.detect(fakefile) is True
+
+
+@pytest.mark.parametrize('filename', [
+    'other.rs',
+    'otherrst',
+    'other',
+    'other.',
+])
+def test_does_not_detect_other_extensions(fakefile, filename):
+    fakefile.name = filename
+    handler = mfr_rst.Handler()
+    assert handler.detect(fakefile) is False
+
+
+def test_render_rst_returns_render_result():
+    with open('mfr_rst/fixtures/test.rst') as fp:
+        result = mfr_rst.render.render_rst(fp)
+
+    assert type(result) == mfr.core.RenderResult
+

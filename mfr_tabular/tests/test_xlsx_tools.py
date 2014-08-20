7fb3f910c36658a37175119caf9c9f0850e5ae15
asmacdo <asmacdo@gmail.com>
2014-08-20 15:01:12 -0400

    test xlsx module and fix py3 incompatability with xrange

---
+++ mfr_tabular/tests/test_xlsx_tools.py
@@ -0,0 +1,9 @@
+from mfr_tabular import xlrd_tools
+
+
+def test_xlsx_xlrd():
+    with open('mfr_tabular/tests/fixtures/test.xlsx') as fp:
+        headers, data = xlrd_tools.xlsx_xlrd(fp)
+
+    assert headers[0] == {'field': 'one', 'id': 'one', 'name': 'one'}
+    assert data[0] == {'one': 'a', 'two': 'b', 'three': 'c'}

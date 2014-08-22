b5b6f09c04959fa9209cc9d97010a1b049c40282
asmacdo <asmacdo@gmail.com>
2014-08-22 09:51:21 -0400

    get static uri base from config

--- mfr_pdb/render.py
+++ mfr_pdb/render.py
@@ -1,3 +1,4 @@
+import mfr
 from mfr.core import RenderResult
 from mako.lookup import TemplateLookup
 
@@ -9,13 +10,17 @@ template = TemplateLookup(
 def render_html(fp, **kwargs):
     content = template.render(pdb_file=fp.read())
 
+    assets_uri_base = '{0}/mfr_pdb'.format(mfr.config['STATIC_URL'])
     # assets must be loaded in this order
+    js_assets = [
+        "jquery-1.7.min.js",
+        "Three49custom.js",
+        "GLmol.js",
+    ]
+
     assets = {
-        'js': [
-            "/static/mfr/mfr_pdb/js/jquery-1.7.min.js",
-            "/static/mfr/mfr_pdb/js/Three49custom.js",
-            "/static/mfr/mfr_pdb/js/GLmol.js",
-        ]
+        'js': ['{0}/{1}/{2}'.format(assets_uri_base, 'js', filepath)
+               for filepath in js_assets]
     }
 
     return RenderResult(content, assets)

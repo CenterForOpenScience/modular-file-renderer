f97717b8b55a9786ceee5651afe3086a2796dce2
asmacdo <asmacdo@gmail.com>
2014-08-27 09:13:40 -0400

    make assets a constant and use mfr core to build asset paths

--- mfr_pdb/render.py
+++ mfr_pdb/render.py
@@ -1,26 +1,30 @@
+import os
 import mfr
 from mfr.core import RenderResult
-from mako.lookup import TemplateLookup
+from mako.template import Template
 
-template = TemplateLookup(
-    directories=['mfr_pdb/templates']
-).get_template('pdb.mako')
+
+JS_ASSETS = [
+    "jquery-1.7.min.js",
+    "Three49custom.js",
+    "GLmol.js",
+]
 
 
 def render_html(fp, **kwargs):
+    HERE = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(HERE, 'templates', 'tabular.mako')
+    template = Template(filename=filename)
+
     content = template.render(pdb_file=fp.read())
 
     assets_uri_base = '{0}/mfr_pdb'.format(mfr.config['STATIC_URL'])
     # assets must be loaded in this order
-    js_assets = [
-        "jquery-1.7.min.js",
-        "Three49custom.js",
-        "GLmol.js",
-    ]
 
+    # TODO(asmacdo) replace with mfr.get_assets_from_list
     assets = {
         'js': ['{0}/{1}/{2}'.format(assets_uri_base, 'js', filepath)
-               for filepath in js_assets]
+               for filepath in JS_ASSETS]
     }
 
     return RenderResult(content, assets)

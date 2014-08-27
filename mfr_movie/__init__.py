9630ca820c143e9453d3d490efabbdae6984cf68
asmacdo <asmacdo@gmail.com>
2014-08-27 10:53:21 -0400

    remove blank exporter, if desired, this can be an issue

--- mfr_movie/__init__.py
+++ mfr_movie/__init__.py
@@ -2,9 +2,6 @@
 from mfr.core import FileHandler, get_file_extension
 from mfr_movie.render import render_movie_tag
 
-# TODO(asmacdo) add exporter
-exporters = {}
-
 EXTENSIONS = [
     '.mp4',
     '.avi',
@@ -20,8 +17,6 @@ class Handler(FileHandler):
     renderers = {
         'html': render_movie_tag,
     }
-
-    exporters = exporters
 
     def detect(self, fp):
         return get_file_extension(fp.name) in EXTENSIONS

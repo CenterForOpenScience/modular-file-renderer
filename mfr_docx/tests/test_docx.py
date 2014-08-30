200fed44edbd3364abf31c5a3c96d0eb4869cb26
asmacdo <asmacdo@gmail.com>
2014-08-30 15:23:45 -0400

    use pytest skipif rather than if else

--- mfr_docx/tests/test_docx.py
+++ mfr_docx/tests/test_docx.py
@@ -1,27 +1,43 @@
 import sys
+import pytest
 import mfr
 if not sys.version_info >= (3, 0):
 
     from mfr_docx import Handler as DocxFileHandler
     from mfr_docx.render import render_docx
 
-    def setup_function(func):
-        mfr.register_filehandler(DocxFileHandler)
+else:
+    DocxFileHandler = render_docx = None
 
-    def test_detect_docx(fakefile):
-        # set file's name
-        fakefile.name = 'mydoc.docx'
-        handler = DocxFileHandler()
 
-        assert handler.detect(fakefile) is True
+@pytest.mark.skipif(sys.version_info >= (3, 0),
+                    reason="pydocx 0.3.14 does not support py3")
+def setup_function(func):
+    mfr.register_filehandler(DocxFileHandler)
 
-    def test_detect_nondocx(fakefile):
-        fakefile.name = 'not_doc.odt'
-        handler = DocxFileHandler()
-        assert handler.detect(fakefile) is False
 
-    def test_render_docx():
-        with open('mfr_docx/tests/test.docx') as fp:
-            result = render_docx(fp)
+@pytest.mark.skipif(sys.version_info >= (3, 0),
+                    reason="pydocx 0.3.14 does not support py3")
+def test_detect_docx(fakefile):
+    # set file's name
+    fakefile.name = 'mydoc.docx'
+    handler = DocxFileHandler()
 
-        assert type(result) == mfr.core.RenderResult
+    assert handler.detect(fakefile) is True
+
+
+@pytest.mark.skipif(sys.version_info >= (3, 0),
+                    reason="pydocx 0.3.14 does not support py3")
+def test_detect_nondocx(fakefile):
+    fakefile.name = 'not_doc.odt'
+    handler = DocxFileHandler()
+    assert handler.detect(fakefile) is False
+
+
+@pytest.mark.skipif(sys.version_info >= (3, 0),
+                    reason="pydocx 0.3.14 does not support py3")
+def test_render_docx():
+    with open('mfr_docx/tests/test.docx') as fp:
+        result = render_docx(fp)
+
+    assert type(result) == mfr.core.RenderResult

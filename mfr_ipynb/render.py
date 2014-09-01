0de794587d865984ebbd02c1f39b83a0edd510e5
asmacdo <asmacdo@gmail.com>
2014-09-01 14:44:41 -0400

    remove mako dependency

--- mfr_ipynb/render.py
+++ mfr_ipynb/render.py
@@ -4,10 +4,8 @@ import os.path
 from IPython.nbformat import current as nbformat
 from IPython.config import Config
 from IPython.nbconvert.exporters import HTMLExporter
-from mako.lookup import TemplateLookup
 
 from mfr import config as core_config, RenderResult
-from mfr.core import get_static_path_for_handler
 
 
 c = Config()
@@ -31,18 +29,18 @@ def render_html(file_pointer, **kwargs):
     name, theme = get_metadata(nb)
     body = exporter.from_notebook_node(nb)[0]
 
-    content = render_mako(
-        template_name="ipynb.mako",
-        body=body,
-        file_name=name,
-        css_theme=theme,
-        mathjax_conf=None
-    )
+    HERE = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(HERE, 'templates', 'ipynb.html')
+
+    with open(filename) as template:
+        content = template.read().format(
+            body=body,
+        )
 
     assets = {
         'css': get_stylesheets(
             "mfr_ipynb/css/pygments.css",
-            # TODO(asmacdo) Why do these break the page?
+            # TODO Thses come from nb viewer, but conflict with the page
             # "mfr_ipynb/css/style.min.css",
             # "mfr_ipynb/css/theme/cdp_1.css",
             # "mfr_ipynb/css/theme/css_linalg.css",
@@ -70,19 +68,6 @@ def get_metadata(nb):
         css_theme = None
 
     return name, css_theme
-
-
-def render_mako(template_name, css_theme, file_name, body, mathjax_conf, **kwargs):
-    template_path = os.path.split(get_static_path_for_handler(render_html))[0]
-    template_path = os.path.join(template_path, 'templates')
-    mako_lookup = TemplateLookup(directories=[template_path])
-
-    return mako_lookup.get_template(template_name).render(
-        body=body,
-        file_name=file_name,
-        css_theme=css_theme,
-        mathjax_conf=None
-    )
 
 
 def get_stylesheets(*args):

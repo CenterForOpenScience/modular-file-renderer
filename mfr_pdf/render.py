8eca37a1242545d4ad8d3ca13b4088447af8816b
asmacdo <asmacdo@gmail.com>
2014-09-04 16:09:06 -0400

    move constants out of function

--- mfr_pdf/render.py
+++ mfr_pdf/render.py
@@ -13,6 +13,10 @@ JS_ASSETS = [
 ]
 
 
+HERE = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE = os.path.join(HERE, 'templates', 'pdfpage.html')
+
+
 def render_pdf(fp, src=None):
     """A simple pdf renderer.
 
@@ -20,16 +24,13 @@ def render_pdf(fp, src=None):
     """
     src = src or fp.name
 
-    HERE = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(HERE, 'templates', 'pdfpage.html')
-
     assets_uri_base = '{0}/mfr_pdf'.format(mfr.config['STATIC_URL'])
 
     assets = {
         'js': get_assets_from_list(JS_ASSETS, 'js', assets_uri_base)
     }
 
-    with open(filename) as template:
+    with open(TEMPLATE) as template:
         content = template.read().format(
             url=src,
             STATIC_PATH=core_config['STATIC_URL']

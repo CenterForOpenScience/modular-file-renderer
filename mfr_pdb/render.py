d2b733aea8cf963e740c11ed9d3a8ac6031a99a3
asmacdo <asmacdo@gmail.com>
2014-09-04 16:14:52 -0400

    use core function get_assets_from_list to simplify asset building

--- mfr_pdb/render.py
+++ mfr_pdb/render.py
@@ -1,28 +1,27 @@
 import os
 import mfr
-from mfr.core import RenderResult
+from mfr.core import RenderResult, get_assets_from_list
 
+# assets must be loaded in this order
 JS_ASSETS = [
     "jquery-1.7.min.js",
     "Three49custom.js",
     "GLmol.js",
 ]
 
+HERE = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE = os.path.join(HERE, 'templates', 'pdb.html')
+
 
 def render_html(fp, **kwargs):
-    HERE = os.path.dirname(os.path.abspath(__file__))
-    filename = os.path.join(HERE, 'templates', 'pdb.html')
 
-    with open(filename) as template:
+    with open(TEMPLATE) as template:
         content = template.read().format(pdb_file=fp.read())
 
     assets_uri_base = '{0}/mfr_pdb'.format(mfr.config['STATIC_URL'])
-    # assets must be loaded in this order
 
-    # TODO(asmacdo) replace with mfr.get_assets_from_list
     assets = {
-        'js': ['{0}/{1}/{2}'.format(assets_uri_base, 'js', filepath)
-               for filepath in JS_ASSETS]
+        'js': get_assets_from_list(assets_uri_base, 'js', JS_ASSETS)
     }
 
     return RenderResult(content, assets)

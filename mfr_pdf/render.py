621680e4afaf0f7a143793f97efcc7f5ffc5e428
asmacdo <asmacdo@gmail.com>
2014-09-04 15:51:48 -0400

    simplify and use get_assets_from_list from core

--- mfr_pdf/render.py
+++ mfr_pdf/render.py
@@ -1,26 +1,16 @@
 """PDF renderer module."""
-from mfr.core import RenderResult
-from mfr import config as core_config
 import os
 
+import mfr
 
-def get_assets():
-    """Creates a dictionary of js and css assets"""
+from mfr.core import RenderResult, get_assets_from_list
+from mfr import config as core_config
 
-    static_dir = os.path.join('/static', 'mfr', 'mfr_pdf')
-
-    js_files = [
-        "pdf.js",
-        "compatibility.js",
-        "jquery.min.js",
-    ]
-
-    assets = {}
-    jspath = os.path.join('{static}', 'js', '{fname}')
-    assets['js'] = [jspath.format(static=static_dir, fname=fname)
-                    for fname in js_files]
-
-    return assets
+JS_ASSETS = [
+    "pdf.js",
+    "compatibility.js",
+    "jquery.min.js",
+]
 
 
 def render_pdf(fp, src=None):
@@ -33,10 +23,16 @@ def render_pdf(fp, src=None):
     HERE = os.path.dirname(os.path.abspath(__file__))
     filename = os.path.join(HERE, 'templates', 'pdfpage.html')
 
+    assets_uri_base = '{0}/mfr_pdf'.format(mfr.config['STATIC_URL'])
+
+    assets = {
+        'js': get_assets_from_list(JS_ASSETS, 'js', assets_uri_base)
+    }
+
     with open(filename) as template:
         content = template.read().format(
             url=src,
             STATIC_PATH=core_config['STATIC_URL']
         )
 
-    return RenderResult(content, assets=get_assets())
+    return RenderResult(content, assets=assets)

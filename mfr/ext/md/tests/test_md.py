bad0a70026498d6f769a9528cb5a6ac98046450e
Matt Frazier <maf7sm@virginia.edu>
2015-01-28 16:07:17 -0500

    Fix md test

--- mfr/ext/md/tests/test_md.py
+++ mfr/ext/md/tests/test_md.py
@@ -2,7 +2,7 @@ from mfr.ext.md import Handler, render
 from mock import MagicMock
 
 def test_render_html():
-    fakefile = MagicMock(spec=file)
+    fakefile = MagicMock()
     fakefile.read.return_value = '# foo'
     assert render.render_html(fakefile).content == '<h1>foo</h1>'
     fakefile.read.return_value = '_italic_'

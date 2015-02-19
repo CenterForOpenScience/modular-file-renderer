708698d635b497ca8e577f95fa37bd29f510f7b3
Casey Rollins <cmr3rm@virginia.edu>
2015-02-19 13:55:23 -0500

    Render file from the url encoded src path

--- mfr/ext/pdf/render.py
+++ mfr/ext/pdf/render.py
@@ -1,6 +1,7 @@
 """PDF renderer module."""
 from mfr.core import RenderResult
 import PyPDF2
+import urllib
 
 
 def is_valid(fp):
@@ -23,10 +24,14 @@ def render_pdf(fp, src=None):
     :return: A RenderResult object containing html content and js assets for pdf rendering
     """
     src = src or fp.name
+    url_encoded_src = urllib.quote_plus(src)
 
     if is_valid(fp):
-        print str(fp)
-        content = '<iframe src="/static/mfr/pdf/web/viewer.html?file=%2Fstatic%2Fmfr%2Fpdf%2Ftest%2FLecture.pdf" width="100%" height="600px"/>'
+        content = (
+            '<iframe src="/static/mfr/pdf/web/viewer.html?file={src}" width="100%" height="600px"/>'
+        ).format(src = url_encoded_src)
         return RenderResult(content)
     else:
         return RenderResult("This is not a valid pdf file")
+
+

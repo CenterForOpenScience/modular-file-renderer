b0c3bb2813f85be8c2f8b56dea69d7886c877cc1
icereval <icereval@gmail.com>
2015-05-09 18:02:24 -0400

    osf provider download and url caching

--- mfr/providers/osf/provider.py
+++ mfr/providers/osf/provider.py
@@ -1,52 +1,49 @@
 import os
-import http
-import json
 import asyncio
 import hashlib
 
 import aiohttp
 
-# from waterbutler.core import utils
-# from waterbutler.core import streams
 from mfr.core import provider
-# from waterbutler.core import exceptions
-#
-from mfr.providers.osf import settings
+from mfr.core import streams
 
 
 class OsfProvider(provider.BaseProvider):
 
+    def __init__(self, url):
+        super().__init__(url)
+        self._download_url = None
+
     @asyncio.coroutine
     def metadata(self):
-        # make request to osf, don't follow, for waterbutler url
-        # modify url for metadata
-
-        # TODO: Add Auth Token
-        request = yield from aiohttp.request('GET', self.url, allow_redirects=False)
-        metadata_url = request.headers['location'].replace('/file?', '/data?', 1)
-
+        download_url = yield from self._fetch_download_url()
+        metadata_url = download_url.replace('/file?', '/data?', 1)
         metadata_request = yield from aiohttp.request('GET', metadata_url)
         metadata = yield from metadata_request.json()
-        # metadata = {
+        # e.g.,
+        # metadata = {'data': {
         #     'name': 'blah.pdf',
         #     'extra': {
         #         'revisionId': '1234',
         #     },
-        # }
+        # }}
         _, ext_name = os.path.splitext(metadata['data']['name'])
         # TODO: Add UniqueKey to WaterButler metadata, currently extra data varies per provider ('revisionId', 'version', etc)
         unique_key = u'{}+{}'.format(self.url, metadata['data']['extra']['version'])
         unique_key_hash = hashlib.sha256(unique_key.encode('utf-8')).hexdigest()
         return ext_name, unique_key_hash
 
-
     @asyncio.coroutine
     def download(self):
-        # go to osf url
-        # don't follow
-        # get waterbutler url
+        download_url = yield from self._fetch_download_url()
+        response = yield from aiohttp.request('GET', download_url)
+        return streams.ResponseStreamReader(response)
 
-        from mfr.core.streams import FileStreamReader
-
-        file_pointer = open('/Users/michael/Downloads/cas_proxy_protocol.pdf', 'rb')
-        return FileStreamReader(file_pointer)
+    @asyncio.coroutine
+    def _fetch_download_url(self):
+        if not self._download_url:
+            # TODO: Add auth token to header
+            # make request to osf, don't follow, store waterbutler url
+            request = yield from aiohttp.request('GET', self.url, allow_redirects=False)
+            self._download_url = request.headers['location']
+        return self._download_url

545f1c36005fd1eb67828f53d8fd030df8287c0f
Chris Seto <chriskseto@gmail.com>
2015-05-26 20:37:40 -0400

    Refactor logic to cache to watertbuler providers

--- mfr/server/handlers/render.py
+++ mfr/server/handlers/render.py
@@ -1,5 +1,9 @@
 import os
 import asyncio
+import logging
+
+import waterbutler.core.exceptions
+from waterbutler.providers.filesystem import FileSystemProvider
 
 from mfr.core import utils as core_utils
 from mfr.core.streams.base import StringStream
@@ -10,12 +14,11 @@ from mfr.server.handlers import core
 
 from mfr.core.streams import FileStreamReader
 
+
+logger = logging.getLogger(__name__)
+
+
 class RenderHandler(core.BaseHandler):
-
-    ACTION_MAP = {
-        'GET': 'render',
-    }
-
     @utils.coroutine
     def prepare(self):
         yield from super().prepare()
@@ -32,48 +35,31 @@ class RenderHandler(core.BaseHandler):
     @utils.coroutine
     def get(self):
         """Render a file with the extension"""
-        cached_file_path = os.path.join('/tmp', 'mfr', 'files', self.unique_key)
-        cached_rendition_path = os.path.join('/tmp', 'mfr', 'rendition', self.unique_key)
+        try:
+            cached_stream = yield from self.cache_provider.download(self.unique_path)
+        except waterbutler.core.exceptions.DownloadError as e:
+            assert e.code == 404, 'Non-404 DownloadError {!r}'.format(e)
+            logger.info('No cached file found; Starting render')
+        else:
+            logger.info('Cached file found; Sending downstream')
+            return (yield from self.write_stream(cached_stream))
 
-        os.makedirs(os.path.dirname(cached_file_path), exist_ok=True)
-        os.makedirs(os.path.dirname(cached_rendition_path), exist_ok=True)
+        yield from self.local_cache_provider.upload(
+            (yield from self.provider.download()),
+            self.local_cache_path
+        )
+
         self.extension = core_utils.make_extension(
             self.ext,
             self.url,
-            cached_file_path,
+            self.local_cache_path.full_path,
             '{}://{}/assets'.format(self.request.protocol, self.request.host),
             self.ext,
         )
 
-        # TODO: Remove after changed to write to temp folder then move to final destination
-        if os.path.exists(cached_rendition_path):
-            os.remove(cached_rendition_path)
+        loop = asyncio.get_event_loop()
+        rendition = (yield from loop.run_in_executor(None, self.extension.render))
 
-        if not os.path.exists(cached_rendition_path):
-            if self.extension.requires_file:
-                if os.path.exists(cached_file_path):
-                    os.remove(cached_file_path)
-
-                file_stream = yield from self.provider.download()
-                with open(cached_file_path, 'wb') as file_pointer:
-                    chunk = yield from file_stream.read(settings.CHUNK_SIZE)
-                    while chunk:
-                        file_pointer.write(chunk)
-                        chunk = yield from file_stream.read(settings.CHUNK_SIZE)
-
-            loop = asyncio.get_event_loop()
-            rendition = (yield from loop.run_in_executor(None, self.extension.render))
-            rendition_stream = StringStream(rendition)
-            with open(cached_rendition_path, 'wb') as file_pointer:
-                chunk = yield from rendition_stream.read(settings.CHUNK_SIZE)
-                while chunk:
-                    file_pointer.write(chunk)
-                    chunk = yield from rendition_stream.read(settings.CHUNK_SIZE)
-
-        with open(cached_rendition_path, 'rb') as file_pointer:
-            file_stream = FileStreamReader(file_pointer)
-            chunk = yield from file_stream.read(settings.CHUNK_SIZE)
-            while chunk:
-                self.write(chunk)
-                yield from utils.future_wrapper(self.flush())
-                chunk = yield from file_stream.read(settings.CHUNK_SIZE)
+        # TODO Spin off current request
+        yield from self.cache_provider.upload(StringStream(rendition), self.unique_path)
+        yield from self.write_stream(StringStream(rendition))

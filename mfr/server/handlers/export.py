049cc21921dbc61a9857b68aa926f3819265956a
icereval <icereval@gmail.com>
2015-06-24 08:52:31 -0400

    optimize server handlers

--- mfr/server/handlers/export.py
+++ mfr/server/handlers/export.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 import asyncio
 import logging
@@ -15,66 +16,76 @@ logger = logging.getLogger(__name__)
 
 class ExportHandler(core.BaseHandler):
 
+    ALLOWED_METHODS = ['GET']
+
     @waterbutler.server.utils.coroutine
     def prepare(self):
+        if self.request.method not in self.ALLOWED_METHODS:
+            return
+
         yield from super().prepare()
+
         self.format = self.request.query_arguments['format'][0].decode('utf-8')
+        self.cache_file_path = yield from self.cache_provider.validate_path('/export/{}.{}'.format(self.metadata.unique_key, self.format))
+        self.source_file_path = yield from self.local_cache_provider.validate_path('/export/{}'.format(uuid.uuid4()))
+        self.output_file_path = yield from self.local_cache_provider.validate_path('/export/{}.{}'.format(self.source_file_path.name, self.format))
 
     @waterbutler.server.utils.coroutine
     def get(self):
         """Export a file to the format specified via the associated extension library"""
-        cache_file_path = yield from self.cache_provider.validate_path('/export/{}.{}'.format(self.metadata.unique_key, self.format))
-        source_file_path = yield from self.local_cache_provider.validate_path('/export/{}'.format(uuid.uuid4()))
-        output_file_path = yield from self.local_cache_provider.validate_path('/export/{}.{}'.format(source_file_path.name, self.format))
 
         if settings.CACHE_ENABLED:
             try:
-                cached_stream = yield from self.cache_provider.download(cache_file_path)
+                cached_stream = yield from self.cache_provider.download(self.cache_file_path)
             except waterbutler.core.exceptions.DownloadError as e:
                 assert e.code == 404, 'Non-404 DownloadError {!r}'.format(e)
                 logger.info('No cached file found; Starting export')
             else:
                 logger.info('Cached file found; Sending downstream')
                 # TODO: Set Content Disposition Header
+                self._set_headers()
                 return (yield from self.write_stream(cached_stream))
 
         yield from self.local_cache_provider.upload(
             (yield from self.provider.download()),
-            source_file_path
+            self.source_file_path
         )
 
         exporter = utils.make_exporter(
             self.metadata.ext,
             self.metadata,
-            source_file_path.full_path,
-            output_file_path.full_path,
+            self.source_file_path.full_path,
+            self.output_file_path.full_path,
             self.format
         )
 
         loop = asyncio.get_event_loop()
         yield from loop.run_in_executor(None, exporter.export)
 
-        with open(output_file_path.full_path, 'rb') as fp:
-            self.set_header('Content-Disposition', 'attachment;filename="{}"'.format('{}.{}'.format(self.metadata.name.replace('"', '\\"'), self.format)))
-            if self.metadata.content_type:
-                self.set_header('Content-Type', self.metadata.content_type)
+        with open(self.output_file_path.full_path, 'rb') as fp:
+            self._set_headers()
             yield from self.write_stream(waterbutler.core.streams.FileStreamReader(fp))
 
     @waterbutler.server.utils.coroutine
     def on_finish(self):
-        pass
-        # loop = asyncio.get_event_loop()
-        #
-        # if settings.CACHE_ENABLED:
-        #     with open(output_file_path.full_path, 'rb') as fp:
-        #         yield from self.cache_provider.upload(waterbutler.core.streams.FileStreamReader(fp), cache_file_path)
-        #
-        # try:
-        #     os.remove(source_file_path.full_path)
-        # except FileNotFoundError:
-        #     pass
-        #
-        # try:
-        #     os.remove(output_file_path.full_path)
-        # except FileNotFoundError:
-        #     pass
+        if self.request.method not in self.ALLOWED_METHODS:
+            return
+
+        if settings.CACHE_ENABLED:
+            with open(self.output_file_path.full_path, 'rb') as fp:
+                yield from self.cache_provider.upload(waterbutler.core.streams.FileStreamReader(fp), self.cache_file_path)
+
+        try:
+            os.remove(self.source_file_path.full_path)
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.remove(self.output_file_path.full_path)
+        except FileNotFoundError:
+            pass
+
+    def _set_headers(self):
+        self.set_header('Content-Disposition', 'attachment;filename="{}"'.format('{}.{}'.format(self.metadata.name.replace('"', '\\"'), self.format)))
+        if self.metadata.content_type:
+            self.set_header('Content-Type', self.metadata.content_type)
--- mfr/server/handlers/render.py
+++ mfr/server/handlers/render.py
@@ -16,54 +16,63 @@ logger = logging.getLogger(__name__)
 
 class RenderHandler(core.BaseHandler):
 
+    ALLOWED_METHODS = ['GET']
+
     @waterbutler.server.utils.coroutine
     def prepare(self):
+        if self.request.method not in self.ALLOWED_METHODS:
+            return
+
         yield from super().prepare()
+
+        self.cache_file_path = yield from self.cache_provider.validate_path('/render/' + self.metadata.unique_key)
+        self.source_file_path = yield from self.local_cache_provider.validate_path('/render/' + str(uuid.uuid4()))
 
     @waterbutler.server.utils.coroutine
     def get(self):
         """Render a file with the extension"""
-        cache_file_path = yield from self.cache_provider.validate_path('/render/' + self.metadata.unique_key)
-        source_file_path = yield from self.local_cache_provider.validate_path('/render/' + str(uuid.uuid4()))
         renderer = utils.make_renderer(
             self.metadata.ext,
             self.metadata,
             self.url,
-            source_file_path.full_path,
+            self.source_file_path.full_path,
             '{}://{}/assets'.format(self.request.protocol, self.request.host)
         )
 
         if renderer.cache_result and settings.CACHE_ENABLED:
             try:
-                cached_stream = yield from self.cache_provider.download(cache_file_path)
+                cached_stream = yield from self.cache_provider.download(self.cache_file_path)
             except waterbutler.core.exceptions.DownloadError as e:
                 assert e.code == 404, 'Non-404 DownloadError {!r}'.format(e)
                 logger.info('No cached file found; Starting render')
             else:
                 logger.info('Cached file found; Sending downstream')
-                # TODO: Set Content Disposition Header
                 return (yield from self.write_stream(cached_stream))
 
         if renderer.file_required:
             yield from self.local_cache_provider.upload(
                 (yield from self.provider.download()),
-                source_file_path
+                self.source_file_path
             )
 
         loop = asyncio.get_event_loop()
         rendition = (yield from loop.run_in_executor(None, renderer.render))
 
-        if renderer.file_required:
-            try:
-                os.remove(source_file_path.full_path)
-            except FileNotFoundError:
-                pass
-
         # Spin off upload into non-blocking operation
         if renderer.cache_result and settings.CACHE_ENABLED:
             loop.call_soon(
                 asyncio.async,
-                self.cache_provider.upload(waterbutler.core.streams.StringStream(rendition), cache_file_path)
+                self.cache_provider.upload(waterbutler.core.streams.StringStream(rendition), self.cache_file_path)
             )
 
         yield from self.write_stream(waterbutler.core.streams.StringStream(rendition))
+
+    @waterbutler.server.utils.coroutine
+    def on_finish(self):
+        if self.request.method not in self.ALLOWED_METHODS:
+            return
+
+        try:
+            os.remove(self.source_file_path.full_path)
+        except FileNotFoundError:
+            pass

d933f76edb5b594966661cb0fcff8e971028b2a0
icereval <icereval@gmail.com>
2015-06-24 13:42:59 -0400

    image exporter will default to looking at the file extension

--- mfr/extensions/image/export.py
+++ mfr/extensions/image/export.py
@@ -7,10 +7,8 @@ from mfr.core import exceptions
 class ImageExporter(extension.BaseExporter):
 
     def export(self):
-        # Pillow will not recognize the jpg extension
-        format = 'jpeg' if self.format.lower() == 'jpg' else self.format
         try:
             image = Image.open(self.source_file_path)
-            image.save(self.output_file_path, format=format)
+            image.save(self.output_file_path)
         except UnicodeDecodeError:
             raise exceptions.ExporterError('Unable to export the file in the requested format, please try again later.', code=400)

cd3930e68bef7a1a6640d5357ac55ddf036eab5e
Jonathon Love <jon@thon.cc>
2015-09-11 15:44:00 +0200

    Added JASP file renderer

---
+++ mfr/extensions/jasp/html_processor.py
@@ -0,0 +1,72 @@
+
+from io import StringIO
+from html.parser import HTMLParser
+
+import base64
+
+class HTMLProcessor(HTMLParser):
+
+    # The HTMLProcessor replaces the src attribute in <image> tags with the base64 equivalent
+    # The image content from the zip_file (specified with set_src_source())
+    # It also strips <script> and <object> tags from the HTML (potential attack vectors)
+
+    html = StringIO()  # buffer for the processed HTML
+    zip_file = None
+
+    def set_src_source(self, zip_file):
+        self.zip_file = zip_file
+
+    def handle_starttag(self, tag, attrs):
+
+        if tag == "script" or tag == "object":  # filter scripts and objects (attack vectors)
+            return
+
+        self.html.write("<")
+        self.html.write(tag)
+
+        for attr in attrs:
+
+            self.html.write(" ")
+            self.html.write(attr[0])
+
+            if attr[1] != None:
+
+                self.html.write('="')
+
+                if attr[0] == "src":
+                    self._insert_data_uri(attr[1])
+                else:
+                    self.html.write(attr[1])
+
+                self.html.write('"')
+
+        self.html.write('>')
+
+    def _insert_data_uri(self, src):
+
+        if self.zip_file == None:
+            return
+
+        with self.zip_file.open(src) as src_file:
+            src_data = src_file.read()
+            src_b64  = base64.b64encode(src_data)
+
+            self.html.write("data:image/png;base64,")
+            self.html.write(src_b64.decode("utf-8"))
+
+    def handle_endtag(self, tag):
+
+        if tag == "script" or tag == "object":
+            return
+
+        self.html.write("</")
+        self.html.write(tag)
+        self.html.write(">")
+
+    def handle_data(self, data):
+        self.html.write(data)
+
+    def final_html(self):
+        return self.html.getvalue()
+
+
---
+++ mfr/extensions/jasp/render.py
@@ -0,0 +1,94 @@
+import os
+
+from mako.lookup import TemplateLookup
+from mfr.core import extension
+from mfr.core.exceptions import RendererError
+from zipfile import ZipFile, BadZipFile
+from distutils.version import LooseVersion
+
+from .html_processor import HTMLProcessor
+
+class JASPRenderer(extension.BaseRenderer):
+
+    TEMPLATE = TemplateLookup(
+        directories=[
+            os.path.join(os.path.dirname(__file__), 'templates')
+        ]).get_template('viewer.mako')
+
+    MESSAGE_FILE_CORRUPT = "This JASP file is corrupt, and cannot be viewed"
+    MESSAGE_FILE_TO_OLD  = "This JASP file was created in an early version of JASP, and cannot be previewed"
+
+    def render(self):
+        try:
+            with ZipFile(self.file_path) as zip_file:
+                self._check_file(zip_file)
+                body = self._render_html(zip_file, self.metadata.ext)
+                return self.TEMPLATE.render(base=self.assets_url, body=body)
+        except BadZipFile:
+            raise RendererError(self.MESSAGE_FILE_CORRUPT)
+
+    @property
+    def file_required(self):
+        return True
+
+    @property
+    def cache_result(self):
+        return True
+
+    def _render_html(self, zip_file, ext, *args, **kwargs):
+
+        index = None
+
+        try:
+            with zip_file.open("index.html") as index_data:
+                index = index_data.read().decode("utf-8")
+
+        except KeyError:
+            raise RendererError(self.MESSAGE_FILE_CORRUPT)
+
+        processor = HTMLProcessor()
+        processor.set_src_source(zip_file)
+        processor.feed(index)
+
+        return processor.final_html()
+
+
+    def _check_file(self, zip_file):
+        
+        # Extract manifest file content
+
+        try:
+            with zip_file.open("META-INF/MANIFEST.MF") as manifest_data:
+                manifest = manifest_data.read().decode("utf-8")
+        except KeyError:
+            raise RendererError(self.MESSAGE_FILE_CORRUPT)
+
+        lines = manifest.split("\n")
+
+        # Search for Data-Archive-Version
+
+        for line in lines:
+            keyValue = line.split(":")
+            if len(keyValue) == 2:
+                key   = keyValue[0].strip()
+                value = keyValue[1].strip()
+
+                if key == "Data-Archive-Version":
+                    dataArchiveVersion = value
+                    break
+        else:
+            raise RendererError(self.MESSAGE_FILE_CORRUPT)
+
+        try: 
+            dataArchiveVersion = LooseVersion(dataArchiveVersion)
+        except ValueError:
+            raise RendererError(self.MESSAGE_FILE_CORRUPT)
+
+        # Check that the file is new enough (contains preview content)
+
+        if dataArchiveVersion < LooseVersion("1.0.2"):
+            raise RendererError(self.MESSAGE_FILE_TO_OLD)        
+
+        return True
+        
+

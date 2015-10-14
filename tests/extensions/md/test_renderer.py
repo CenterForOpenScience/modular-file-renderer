b0a0fa2b636a7b4775c11943aa82766b1ae65c27
TomBaxter <tkb608@gmail.com>
2015-10-14 12:37:33 -0400

    Add extention to convert html tags to character entities Expand test

--- tests/extensions/md/test_renderer.py
+++ tests/extensions/md/test_renderer.py
@@ -51,5 +51,35 @@ class TestMdRenderer:
         metadata = ProviderMetadata('test', '.md', 'text/plain', '1234', 'http://wb.osf.io/file/test.md?token=1234')
         renderer = MdRenderer(metadata, test_md_file_path, url, assets_url, export_url)
         body = renderer.render()
-        assert '<ul>\n<li>one</li>\n<li>two</li>\n</ul>' in body
+        inbody = """
+<h1>Heading</h1>
+<h2>Sub-heading</h2>
+<h3>Another deeper heading</h3>
+<p>Paragraphs are separated
+by a blank line.</p>
+<p>Leave 2 spaces at the end of a line to do a<br />
+line break</p>
+<p>Text attributes <em>italic</em>, <strong>bold</strong>, 
+<code>monospace</code>.</p>
+<p>A <a href="http://example.com">link</a>.
+[28]</p>
+<p>Shopping list:</p>
+<ul>
+<li>apples</li>
+<li>oranges</li>
+<li>pears</li>
+</ul>
+<p>Numbered list:</p>
+<ol>
+<li>apples</li>
+<li>oranges</li>
+<li>pears</li>
+</ol>
+<p>The rain---not the reign---in
+Spain.</p>
+<p>&lt;script&gt;
+alert("Hello world");
+&lt;/script&gt;</p>
+"""
+        assert inbody in body
 

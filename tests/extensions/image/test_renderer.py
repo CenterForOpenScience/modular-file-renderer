358ae5059f7f7564902446d01f1e5681a1a6d690
icereval <icereval@gmail.com>
2016-01-23 21:01:50 -0500

    export settings optional, tests to support

--- tests/extensions/image/test_renderer.py
+++ tests/extensions/image/test_renderer.py
@@ -10,8 +10,42 @@ from mfr.extensions.image import settings
 
 class TestImageRenderer:
 
-    def test_render_image(self):
+    def test_render_image_no_export(self):
         settings.EXPORT_TYPE_MAP = {}
+        settings.EXPORT_MAXIMUM_SIZE = None
+        settings.EXPORT_TYPE = None
+
+        url = 'http://osf.io/file/test.png'
+
+        metadata = ProviderMetadata('test', '.png', 'text/plain', '1234', 'http://wb.osf.io/file/test.png?token=1234')
+        renderer = ImageRenderer(metadata, '/tmp/test.png', url, 'http://mfr.osf.io/assets', 'http://this_should_be_ignored')
+
+        body = renderer.render()
+
+        assert '<img style="max-width: 100%;" src="{}">'.format(url) in body
+
+    def test_render_image_export_type(self):
+        settings.EXPORT_TYPE_MAP = {}
+        settings.EXPORT_MAXIMUM_SIZE = None
+        settings.EXPORT_TYPE = 'fake_type'
+
+        url = 'http://osf.io/file/test.png'
+        export_url = furl.furl('http://mfr.osf.io/export')
+        export_url.args['url'] = url
+
+        metadata = ProviderMetadata('test', '.png', 'text/plain', '1234', 'http://wb.osf.io/file/test.png?token=1234')
+        renderer = ImageRenderer(metadata, '/tmp/test.png', url, 'http://mfr.osf.io/assets', export_url.url)
+
+        exported_url = furl.furl(export_url.url)
+        exported_url.args['format'] =  settings.EXPORT_TYPE
+
+        body = renderer.render()
+
+        assert '<img style="max-width: 100%;" src="{}">'.format(exported_url) in body
+
+    def test_render_image_export_size_and_type(self):
+        settings.EXPORT_TYPE_MAP = {}
+        settings.EXPORT_MAXIMUM_SIZE = '1234x4321'
         settings.EXPORT_TYPE = 'fake_type'
 
         url = 'http://osf.io/file/test.png'
@@ -28,8 +62,23 @@ class TestImageRenderer:
 
         assert '<img style="max-width: 100%;" src="{}">'.format(exported_url) in body
 
-    def test_render_image_excluded_export(self):
+    def test_render_image_excluded_export_file_type(self):
         settings.EXPORT_EXCLUSIONS = ['.png']
+        settings.EXPORT_MAXIMUM_SIZE = None
+        settings.EXPORT_TYPE = 'fake_type'
+
+        url = 'http://osf.io/file/test.png'
+
+        metadata = ProviderMetadata('test', '.png', 'text/plain', '1234', 'http://wb.osf.io/file/test.png?token=1234')
+        renderer = ImageRenderer(metadata, '/tmp/test.png', url, 'http://mfr.osf.io/assets', 'http://this_should_be_ignored')
+
+        body = renderer.render()
+
+        assert '<img style="max-width: 100%;" src="{}">'.format(url) in body
+
+    def test_render_image_export_maximum(self):
+        settings.EXPORT_EXCLUSIONS = ['.png']
+        settings.EXPORT_MAXIMUM_SIZE = '1234x4321'
         settings.EXPORT_TYPE = 'fake_type'
 
         url = 'http://osf.io/file/test.png'

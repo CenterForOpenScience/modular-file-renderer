05fc10ef517ae9fb809b29ae6e4fc2ff6eca6659
icereval <icereval@gmail.com>
2016-01-23 21:01:50 -0500

    export settings optional, tests to support

--- mfr/extensions/image/render.py
+++ mfr/extensions/image/render.py
@@ -20,7 +20,13 @@ class ImageRenderer(extension.BaseRenderer):
             return self.TEMPLATE.render(base=self.assets_url, url=self.url)
 
         exported_url = furl.furl(self.export_url)
-        exported_url.args['format'] = '{}.{}'.format(settings.EXPORT_MAXIMUM_SIZE, settings.EXPORT_TYPE)
+        if settings.EXPORT_MAXIMUM_SIZE and settings.EXPORT_TYPE:
+            exported_url.args['format'] = '{}.{}'.format(settings.EXPORT_MAXIMUM_SIZE, settings.EXPORT_TYPE)
+        elif settings.EXPORT_TYPE:
+            exported_url.args['format'] = settings.EXPORT_TYPE
+        else:
+            return self.TEMPLATE.render(base=self.assets_url, url=self.url)
+
         return self.TEMPLATE.render(base=self.assets_url, url=exported_url.url)
 
     @property

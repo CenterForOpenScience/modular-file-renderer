984de2797b05e70d3b4376da62aea9a4f46bd4e3
TomBaxter <tkb608@gmail.com>
2016-07-13 11:42:29 -0400

    Limit codePygmentRenderer to 64k files [#OSF-6670]

--- mfr/extensions/codepygments/render.py
+++ mfr/extensions/codepygments/render.py
@@ -9,6 +9,7 @@ from pygments.util import ClassNotFound
 from mako.lookup import TemplateLookup
 
 from mfr.core import extension, exceptions
+from mfr.extensions.codepygments import cp_exceptions
 
 
 class CodePygmentsRenderer(extension.BaseRenderer):
@@ -25,6 +26,9 @@ class CodePygmentsRenderer(extension.BaseRenderer):
         self.metrics.add('pygments_version', pygments.__version__)
 
     def render(self):
+        file_size = os.path.getsize(self.file_path)
+        if file_size > 65536:
+            raise(cp_exceptions.FileToLargeException('Text files larger than 64kb are not rendered; please download the file to view.'))
         with open(self.file_path, 'rb') as fp:
             body = self._render_html(fp, self.metadata.ext)
             return self.TEMPLATE.render(base=self.assets_url, body=body)

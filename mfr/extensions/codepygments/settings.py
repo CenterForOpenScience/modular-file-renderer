cf0169874940c394309643256a3671cdb1de4d14
TomBaxter <tkb608@gmail.com>
2016-10-13 12:20:51 -0400

    Updated to comply with docker-compose support for environment settings Updated test to look for more specific error [OSF-6670]

--- mfr/extensions/codepygments/settings.py
+++ mfr/extensions/codepygments/settings.py
@@ -1,9 +1,5 @@
-try:
-    from mfr import settings
-except ImportError:
-    settings = {}
+from mfr import settings
 
-config = settings.get('CODEPYGMENTS_EXTENSION_CONFIG', {})
-
+config = settings.child('CODEPYGMENTS_EXTENSION_CONFIG')
 
 MAX_SIZE = config.get('MAX_SIZE', 65536)

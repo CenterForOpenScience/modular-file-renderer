0f91adbd645faeb9b7f9c277f78b27d58e040efb
Fitz Elliott <fitz@cos.io>
2017-01-19 15:10:19 -0500

    convert unsupported extension errors to new system
     * File types that are not yet supported by MFR trigger exceptions
       when attempting to load a non-existent renderer or exporter driver
       error. Those errors are of particular interest, so handle them
       specially. Convert the driver manger for providers to use a common
       base class.

--- mfr/core/exceptions.py
+++ mfr/core/exceptions.py
@@ -1,5 +1,7 @@
 import waterbutler.core.exceptions
 
+from mfr import settings
+
 
 class PluginError(waterbutler.core.exceptions.PluginError):
     """The MFR related errors raised from a plugin should inherit from PluginError
@@ -91,4 +93,68 @@ class MetadataError(ProviderError):
             'metadata_url': self.metadata_url,
             'response': self.response
         }])
+
+
+class DriverManagerError(PluginError):
+
+    __TYPE = 'drivermanager'
+
+    def __init__(self, message, *args, namespace: str='', name: str='', invoke_on_load: bool=None,
+                 invoke_args: dict=None, **kwargs):
+        super().__init__(message, *args, **kwargs)
+
+        self.namespace = namespace
+        self.name = name
+        self.invoke_on_load = invoke_on_load
+        self.invoke_args = invoke_args or {}
+
+        self.attr_stack.append([self.__TYPE, {
+            'namespace': self.namespace,
+            'name': self.name,
+            'invoke_on_load': self.invoke_on_load,
+            'invoke_args': self.invoke_args,
+        }])
+
+
+class MakeProviderError(DriverManagerError):
+    """Thrown when MFR can't find an applicable provider class.  This indicates programmer error,
+    so ``code`` defaults to ``500``."""
+
+    def __init__(self, message, *args, code: int=500, **kwargs):
+        super().__init__(message, *args, code=code, **kwargs)
+
+
+class UnsupportedExtensionError(DriverManagerError):
+    """When make_renderer and make_exporter fail, it's usually because MFR doesn't support that
+    extension yet.  This error inherits from DriverManagerError (since it's the DriverManager that
+    trips this) and includes a handler_type argsument
     """
+
+    __TYPE = 'unsupported_extension'
+
+    def __init__(self, *args, code: int=400, handler_type: str='', **kwargs):
+        super().__init__(*args, code=code, **kwargs)
+
+        self.handler_type = handler_type
+
+        self.attr_stack.append([self.__TYPE, {'handler_type': self.handler_type}])
+
+
+class MakeRendererError(UnsupportedExtensionError):
+    """The MFR related errors raised from a :def:`mfr.core.utils.make_renderer` should inherit from
+    MakeRendererError
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(settings.UNSUPPORTED_RENDER_MSG, *args, handler_type='renderer',
+                         **kwargs)
+
+
+class MakeExporterError(UnsupportedExtensionError):
+    """The MFR related errors raised from a :def:`mfr.core.utils.make_exporter` should inherit from
+    MakeExporterError
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(settings.UNSUPPORTED_EXPORTER_MSG, *args, handler_type='exporter',
+                         **kwargs)

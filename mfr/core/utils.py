887b954894f557846c1156d271716b869a861707
John Tordoff <john@cos.io>
2017-02-02 10:03:33 -0500

    Add maximum file size limit for rendering files with certain extensions.

--- mfr/core/utils.py
+++ mfr/core/utils.py
@@ -96,3 +96,10 @@ def make_renderer(name, metadata, file_path, url, assets_url, export_url):
                 'export_url': export_url,
             }
         )
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1000.0:
+            return '%3.1f%s%s' % (num, unit, suffix)
+        num /= 1000.0
+    return '%.1f%s%s' % (num, 'Y', suffix)

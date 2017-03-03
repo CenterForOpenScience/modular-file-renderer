fd602f928cfe02910f101406eaee86f6f8744aa9
John Tordoff <john@cos.io>
2017-03-03 09:28:59 -0500

    add zip rendering and tests
     * Render .zip files as a listing of the contents.  Each entry
       includes the name, timestamp, and size of the file.  macOS-specific
       directories are excluded from the listing.
    
     [SVCS-248]

--- mfr/core/utils.py
+++ mfr/core/utils.py
@@ -98,6 +98,9 @@ def make_renderer(name, metadata, file_path, url, assets_url, export_url):
         )
 
 def sizeof_fmt(num, suffix='B'):
+    if abs(num) < 1000:
+        return '%3.0f%s' % (num, suffix)
+
     for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1000.0:
             return '%3.1f%s%s' % (num, unit, suffix)

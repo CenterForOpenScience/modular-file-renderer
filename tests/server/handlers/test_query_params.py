94ef13643f4e147c7b813916e2d942e373a2a38c
John Tordoff <john@cos.io>
2017-05-15 10:16:25 -0400

    Make invalid query params give 400 and add unit tests

---
+++ tests/server/handlers/test_query_params.py
@@ -0,0 +1,28 @@
+from http import HTTPStatus
+
+import pytest
+from tornado import testing
+from tornado.httpclient import HTTPError
+
+from tests import utils
+
+
+class TestRenderHandler(utils.HandlerTestCase):
+
+    @testing.gen_test
+    def test_format_url(self):
+
+        with pytest.raises(HTTPError) as e:
+            yield self.http_client.fetch(self.get_url('/export'), method='GET')
+        assert e.value.message == 'Bad Request'
+        assert e.value.code == HTTPStatus.BAD_REQUEST
+
+        with pytest.raises(HTTPError) as e:
+            yield self.http_client.fetch(self.get_url('/export?format=pdf'), method='GET')
+        assert e.value.message == 'Bad Request'
+        assert e.value.code == HTTPStatus.BAD_REQUEST
+
+        with pytest.raises(HTTPError) as e:
+            yield self.http_client.fetch(self.get_url('/export?url=http://test.com'), method='GET')
+        assert e.value.message == 'Bad Request'
+        assert e.value.code == HTTPStatus.BAD_REQUEST

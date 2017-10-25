433f135c944d6259a17ecfd442fc7b3afd5e8888
Addison Schiller <acschiller@gmail.com>
2017-10-25 11:30:57 -0400

    Changing test to be cleaner

--- tests/documentation/test_entrypoints.py
+++ tests/documentation/test_entrypoints.py
@@ -1,23 +1,14 @@
 import os
 import pytest
-
+import pkg_resources
 
 class TestEntryPoints:
 
     def test_entry_points(self):
+
         parent_dir = os.pardir
         readme_path = os.path.join(os.path.dirname((parent_dir)), 'supportedextensions.md')
         with open(readme_path, 'r') as file:
             readme_ext = [line.strip()[2:] for line in file if '*' in line]
-        setup_path = os.path.join(os.path.dirname((parent_dir)), 'setup.py')
-        with open(setup_path, 'r') as file:
-            parse = False
-            setup_ext = []
-            for line in file:
-                if parse and '#' not in line and ' = ' in line:
-                    setup_ext.append(line.strip().split(' = ')[0][1:])
-                # after you see this line its okay to start parsing
-                if '\'mfr.renderers\': [' in line:
-                    parse = True
-        setup_ext.remove('none')
-        assert sorted(setup_ext) == sorted(readme_ext)
+        for ep in pkg_resources.iter_entry_points(group='mfr.exporters'):
+            assert ep.name in readme_ext

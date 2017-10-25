98b60bd805b9f8e546a4580c0523a1282c337315
Addison Schiller <acschiller@gmail.com>
2017-10-25 12:16:04 -0400

    fix typo and add none check

--- tests/documentation/test_entrypoints.py
+++ tests/documentation/test_entrypoints.py
@@ -10,5 +10,6 @@ class TestEntryPoints:
         readme_path = os.path.join(os.path.dirname((parent_dir)), 'supportedextensions.md')
         with open(readme_path, 'r') as file:
             readme_ext = [line.strip()[2:] for line in file if '*' in line]
-        for ep in pkg_resources.iter_entry_points(group='mfr.exporters'):
-            assert ep.name in readme_ext
+        for ep in pkg_resources.iter_entry_points(group='mfr.renderers'):
+            if ep.name != 'none':
+                assert ep.name in readme_ext

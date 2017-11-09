fba19c53a93d28f11710499cdba9a0878d4de565
Fitz Elliott <fitz@cos.io>
2017-11-09 11:59:00 -0500

    remove invalid attributes from iframe
     * These were added while trying to enable scrolling, but don't
       actually do anything.  It probably triggered some sort of cache
       rebuild that got scrolling working in local dev.

--- mfr/server/static/js/mfr.js
+++ mfr/server/static/js/mfr.js
@@ -80,9 +80,6 @@
             self.pymParent.iframe.setAttribute('allowfullscreen', '');
             self.pymParent.iframe.setAttribute('webkitallowfullscreen', '');
             self.pymParent.iframe.setAttribute('scrolling', 'yes');
-            self.pymParent.iframe.setAttribute('horizontalscrolling', 'yes');
-            self.pymParent.iframe.setAttribute('verticalscrolling', 'yes');
-            self.pymParent.iframe.setAttribute('overflow', 'scroll');
 
             self.pymParent.el.appendChild(self.spinner);
             $(self.pymParent.iframe).on('load', function () {

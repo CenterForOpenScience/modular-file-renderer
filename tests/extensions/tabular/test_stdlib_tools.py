0ff858931090ae6dfd9e718628f9ae9fc05879dc
Addison Schiller <acschiller@gmail.com>
2017-11-16 14:39:54 -0500

    Add tests

---
+++ tests/extensions/tabular/test_stdlib_tools.py
@@ -0,0 +1,51 @@
+import os
+from collections import OrderedDict
+
+import pytest
+
+from mfr.extensions.tabular.libs import stdlib_tools
+from mfr.extensions.tabular.exceptions import EmptyTableError
+
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestTabularStdlibTools:
+
+    def test_csv_stdlib(self):
+        with open(os.path.join(BASE, 'files', 'test.csv')) as fp:
+            sheets = stdlib_tools.csv_stdlib(fp)
+
+        sheet = sheets.popitem()[1]
+        assert sheet[0] == [
+            {'id': 'one', 'field': 'one', 'name': 'one', 'sortable': True},
+            {'id': 'two', 'field': 'two', 'name': 'two', 'sortable': True},
+            {'id': 'three', 'field': 'three', 'name': 'three', 'sortable': True}
+        ]
+        assert sheet[1][0] == OrderedDict([('one', 'à'), ('two', 'b'), ('three', 'c')])
+        assert sheet[1][1] == OrderedDict([('one', '1'), ('two', '2'), ('three', '3')])
+
+    def test_tsv_stdlib(self):
+        with open(os.path.join(BASE, 'files', 'test.tsv')) as fp:
+            sheets = stdlib_tools.tsv_stdlib(fp)
+
+        sheet = sheets.popitem()[1]
+        assert sheet[0] == [
+            {'id': 'one', 'field': 'one', 'name': 'one', 'sortable': True},
+            {'id': 'two', 'field': 'two', 'name': 'two', 'sortable': True},
+            {'id': 'three', 'field': 'three', 'name': 'three', 'sortable': True}
+        ]
+        assert sheet[1][0] == OrderedDict([('one', 'a'), ('two', 'b'), ('three', 'c')])
+        assert sheet[1][1] == OrderedDict([('one', '1'), ('two', '2'), ('three', '3')])
+
+    def test_tsv_stdlib_exception_raises(self):
+        with open(os.path.join(BASE, 'files', 'invalid.tsv')) as fp:
+            with pytest.raises(EmptyTableError) as e:
+                stdlib_tools.tsv_stdlib(fp)
+                assert e.value.code == 400
+
+    def test_csv_stdlib_exception_raises(self):
+        with open(os.path.join(BASE, 'files', 'invalid.csv')) as fp:
+            with pytest.raises(EmptyTableError) as e:
+                stdlib_tools.tsv_stdlib(fp)
+                assert e.value.code == 400

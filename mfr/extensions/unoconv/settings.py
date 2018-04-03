eee24a69eecd6a37fc96ec335edec6304347c883
Fitz Elliott <fitz@cos.io>
2018-04-03 17:00:08 -0400

    unoconv: add timeout to kill process after 60s
     * When a libreoffice convert process takes longer than 60s, kill the
       process and throw an error.  Libreoffice occasionally hangs
       indefinitely, causing MFR requests to never finish.

--- mfr/extensions/unoconv/settings.py
+++ mfr/extensions/unoconv/settings.py
@@ -6,6 +6,7 @@ from mfr import settings
 config = settings.child('UNOCONV_EXTENSION_CONFIG')
 
 UNOCONV_BIN = config.get('UNOCONV_BIN', '/usr/local/bin/unoconv')
+UNOCONV_TIMEOUT = int(config.get('UNOCONV_TIMEOUT', 60))
 
 ADDRESS = config.get('SERVER', os.environ.get('UNOCONV_PORT_2002_TCP_ADDR', '127.0.0.1'))
 PORT = config.get('PORT', os.environ.get('UNOCONV_PORT_2002_TCP_PORT', '2002'))

05408e9d7ad0b5f0988b90cc9f2bb22539979856
Josh Bird <joshua.thomas.bird@gmail.com>
2018-05-11 13:43:37 -0400

    Add timeouts for exports
    All three of the current exporters use a subprocess call to perform the
    export. To prevent the process from hanging, we add a condition to kill
    th process after a specified period of time.

--- mfr/extensions/jsc3d/settings.py
+++ mfr/extensions/jsc3d/settings.py
@@ -3,6 +3,7 @@ from mfr import settings
 config = settings.child('JSC3D_EXTENSION_CONFIG')
 
 FREECAD_BIN = config.get('FREECAD_BIN', '/usr/bin/freecadcmd')
-FREECAD_CONVERT_SCRIPT = config.get('FREECAD_CONVERT_SCRIPT', '/code/mfr/extensions/jsc3d/freecad_converter.py')
+CONVERSION_SCRIPT = config.get('FREECAD_CONVERT_SCRIPT', '/code/mfr/extensions/jsc3d/freecad_converter.py')
+TIMEOUT = int(config.get('FREECAD_TIMEOUT', 30))  # In seconds
 EXPORT_TYPE = config.get('EXPORT_TYPE', '.stl')
 EXPORT_EXCLUSIONS = config.get('EXPORT_EXCLUSIONS', '.3ds .stl .obj .ctm').split(' ')
--- mfr/extensions/tabular/utilities.py
+++ mfr/extensions/tabular/utilities.py
@@ -1,9 +1,14 @@
 import re
-import subprocess
+from http import HTTPStatus
+from subprocess import (check_call,
+                        TimeoutExpired,
+                        CalledProcessError)
 from tempfile import NamedTemporaryFile
 
-from mfr.core import exceptions
-from mfr.extensions.tabular import compat, settings
+from mfr.extensions.tabular import compat
+from mfr.core.exceptions import SubprocessError
+from mfr.extensions.tabular.settings import (PSPP_CONVERT_BIN,
+                                             PSPP_CONVERT_TIMEOUT)
 
 
 def header_population(headers):
@@ -48,20 +53,33 @@ def sav_to_csv(fp):
     """
     csv_file = NamedTemporaryFile(mode='w+b', suffix='.csv')
     try:
-        subprocess.check_call([
-            settings.PSPP_CONVERT_BIN,
-            fp.name,
-            csv_file.name,
-        ])
-    except subprocess.CalledProcessError as err:
-        raise exceptions.SubprocessError(
+        check_call(
+            [PSPP_CONVERT_BIN, fp.name, csv_file.name],
+            timeout=PSPP_CONVERT_TIMEOUT,
+        )
+    except CalledProcessError as err:
+        raise SubprocessError(
             'Unable to convert the SPSS file to CSV, please try again later.',
-            code=500,
+            code=HTTPStatus.INTERNAL_SERVER_ERROR,
             process='pspp',
             cmd=str(err.cmd),
             returncode=err.returncode,
             path=fp.name,
             extension='sav',
             exporter_class='tabular',
+        )
+    except TimeoutExpired as err:
+        # The return code 52 is not the error code returned by the
+        # subprocess, but the error given to it by this waterbutler
+        # processs, for timing out.
+        raise SubprocessError(
+            'CSV Conversion timed out.',
+            code=HTTPStatus.GATEWAY_TIMEOUT,
+            process='pspp',
+            cmd=str(err.cmd),
+            returncode=52,
+            path=fp.name,
+            extension='sav',
+            exporter_class='tabular'
         )
     return csv_file

24c04ff77715e5e1a5d31f0c2b8225a6e061e3e6
Josh Bird <joshua.thomas.bird@gmail.com>
2018-05-11 14:38:41 -0400

    Make jsc3d async
    Changes the jsc3d exporter to run async without needing to create
    another thread

--- mfr/extensions/jsc3d/export.py
+++ mfr/extensions/jsc3d/export.py
@@ -1,4 +1,15 @@
-import os
+from asyncio import (
+    TimeoutError,
+    create_subprocess_exec,
+    wait_for
+)
+from asyncio.subprocess import (
+    DEVNULL
+)
+from os.path import (
+    basename,
+    splitext
+)
 import shutil
 import subprocess
 from http import HTTPStatus
@@ -6,8 +17,11 @@ from tempfile import NamedTemporaryFile
 
 from mfr.core import exceptions
 from mfr.core.extension import BaseExporter
-from mfr.extensions.jsc3d.settings import (FREECAD_BIN,
-                                           FREECAD_CONVERT_SCRIPT)
+from mfr.extensions.jsc3d.settings import (
+    FREECAD_BIN,
+    TIMEOUT,
+    FREECAD_CONVERT_SCRIPT
+)
 
 
 class JSC3DExporter(BaseExporter):
@@ -16,29 +30,35 @@ class JSC3DExporter(BaseExporter):
         super().__init__(*args, **kwargs)
 
     async def export(self):
-        try:
-            # alter original source file with a named temp file, so freecad can read it properly
-            with NamedTemporaryFile(mode='w+b') as temp_source_file:
-                temp_source_file.name = await self.source_file_path + '.step'
-                shutil.copy2(await self.source_file_path, temp_source_file.name)
+        # alter original source file with a named temp file, so freecad can read it properly
+        with NamedTemporaryFile(mode='w+b') as temp_source_file:
+            temp_source_file.name = await self.source_file_path + '.step'
+            shutil.copy2(await self.source_file_path, temp_source_file.name)
+            self.cmd = [
+                FREECAD_BIN,
+                FREECAD_CONVERT_SCRIPT,
+                temp_source_file.name,
+                self.output_file_path,
+                # silence output from freecadcmd
+            ]
+            self.process = await create_subprocess_exec(*self.cmd, stdout=DEVNULL)
+            try:
+                stdout, stderr = await wait_for(self.process.communicate(), timeout=TIMEOUT)
+            except TimeoutError:
+                return self.jsc3d_fail()
+            if self.process.returncode != 0:
+                return self.jsc3d_fail()
 
-                subprocess.check_call([
-                    FREECAD_BIN,
-                    FREECAD_CONVERT_SCRIPT,
-                    temp_source_file.name,
-                    self.output_file_path,
-                    # silence output from freecadcmd
-                ], stdout=subprocess.DEVNULL)
-
-        except subprocess.CalledProcessError as err:
-            name, extension = os.path.splitext(os.path.split(await self.source_file_path)[-1])
-            raise exceptions.SubprocessError(
-                'Unable to export the file in the requested format, please try again later.',
-                process='freecad',
-                cmd=str(err.cmd),
-                returncode=err.returncode,
-                path=str(await self.source_file_path),
-                code=HTTPStatus.BAD_REQUEST,
-                extension=extension or '',
-                exporter_class='jsc3d',
-            )
+    def jsc3d_fail(self):
+        path = getattr(self, '_source_file_path', '')
+        name, extension = splitext(basename(path))
+        raise exceptions.SubprocessError(
+            'Unable to export the file in the requested format, please try again later.',
+            process='freecad',
+            cmd=str(*self.cmd),
+            returncode=self.process.returncode,
+            path=str(path),
+            code=HTTPStatus.BAD_REQUEST,
+            extension=extension or '',
+            exporter_class='jsc3d',
+        )

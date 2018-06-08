9547ce4c5327c70a46b3c59ba8d0729a765fa49c
Fitz Elliott <fitz@cos.io>
2018-06-08 12:34:27 -0400

    inline test fixtures
     * Make tests a little more obvious/explicit by inlining single-use
       fixtures.

--- tests/extensions/pdf/test_renderer.py
+++ tests/extensions/pdf/test_renderer.py
@@ -13,21 +13,9 @@ def metadata():
 
 
 @pytest.fixture
-def metadata_2():
-    return ProviderMetadata('te\'st', '.pdf', 'text/plain', '1234',
-                            'http://wb.osf.io/file/te\'st.pdf?token=1234')
-
-
-@pytest.fixture
 def tif_metadata():
     return ProviderMetadata('test', '.tif', 'text/plain', '1234',
                             'http://wb.osf.io/file/test.tif?token=1234')
-
-
-@pytest.fixture
-def docx_metadata():
-    return ProviderMetadata('te\'st', '.docx', 'text/plain','1234',
-                            'http://mfr.osf.io/export?url=http://osf.io/file/te\'st.pdf')
 
 
 @pytest.fixture
@@ -36,18 +24,8 @@ def file_path():
 
 
 @pytest.fixture
-def file_path_2():
-    return '/tmp/te\'st.pdf'
-
-
-@pytest.fixture
 def tif_file_path():
     return '/tmp/test.tif'
-
-
-@pytest.fixture
-def docx_file_path():
-    return '/tmp/te\'st.docx'
 
 
 @pytest.fixture
@@ -56,18 +34,8 @@ def url():
 
 
 @pytest.fixture
-def url_2():
-    return 'http://osf.io/file/te\'st.pdf'
-
-
-@pytest.fixture
 def tif_url():
     return 'http://osf.io/file/test.tif'
-
-
-@pytest.fixture
-def docx_url():
-    return 'http://osf.io/file/te\'st.tif'
 
 
 @pytest.fixture
@@ -81,28 +49,13 @@ def export_url():
 
 
 @pytest.fixture
-def export_url_2():
-    return 'http://mfr.osf.io/export?url=http://osf.io/file/te\'st.pdf'
-
-
-@pytest.fixture
 def renderer(metadata, file_path, url, assets_url, export_url):
     return PdfRenderer(metadata, file_path, url, assets_url, export_url)
 
 
 @pytest.fixture
-def renderer_2(metadata_2, file_path_2, url_2, assets_url, export_url_2):
-    return PdfRenderer(metadata_2, file_path_2, url_2, assets_url, export_url_2)
-
-
-@pytest.fixture
 def tif_renderer(tif_metadata, tif_file_path, tif_url, assets_url, export_url):
     return PdfRenderer(tif_metadata, tif_file_path, tif_url, assets_url, export_url)
-
-
-@pytest.fixture
-def docx_renderer(docx_metadata, docx_file_path, docx_url, assets_url, export_url_2):
-    return PdfRenderer(docx_metadata, docx_file_path, docx_url, assets_url, export_url_2)
 
 
 class TestPdfRenderer:
@@ -113,14 +66,21 @@ class TestPdfRenderer:
         assert '<div id="viewer" class="pdfViewer"></div>' in body
         assert 'DEFAULT_URL = \'{}\''.format(metadata.download_url) in body
 
-    def test_render_pdf_with_single_quote_in_name(self, renderer_2, metadata_2, assets_url):
+    def test_render_pdf_with_single_quote_in_name(self, assets_url):
 
-        body = renderer_2.render()
-        safe_download_url = escape_url_for_template(metadata_2.download_url, logs=False)
+        download_url = 'http://wb.osf.io/file/te\'st.pdf?token=1234'
+        safe_download_url = 'http://wb.osf.io/file/te%27st.pdf?token=1234'
+
+        metadata = ProviderMetadata('te\'st', '.pdf', 'text/plain', '1234', download_url)
+        renderer = PdfRenderer(metadata, '/tmp/te\'st.pdf', 'http://osf.io/file/te\'st.pdf',
+                               assets_url,
+                               'http://mfr.osf.io/export?url=http://osf.io/file/te\'st.pdf')
+
+        body = renderer.render()
 
         assert '<base href="{}/{}/web/" target="_blank">'.format(assets_url, 'pdf') in body
         assert '<div id="viewer" class="pdfViewer"></div>' in body
-        assert 'DEFAULT_URL = \'{}\''.format(metadata_2.download_url) not in body
+        assert 'DEFAULT_URL = \'{}\''.format(download_url) not in body
         assert 'DEFAULT_URL = \'{}\''.format(safe_download_url) in body
 
     def test_render_tif(self, tif_renderer, assets_url):
@@ -133,13 +93,18 @@ class TestPdfRenderer:
         assert '<div id="viewer" class="pdfViewer"></div>' in body
         assert 'DEFAULT_URL = \'{}\''.format(exported_url.url) in body
 
-    def test_render_docx(self, docx_renderer, assets_url):
+    def test_render_docx(self, assets_url):
 
-        exported_url = furl.furl(docx_renderer.export_url)
-        safe_exported_url = escape_url_for_template(exported_url.url, logs=False)
-        body = docx_renderer.render()
+        export_url = 'http://mfr.osf.io/export?url=http://osf.io/file/te\'st.docx&format=pdf'
+        safe_url =  'http://mfr.osf.io/export?url=http://osf.io/file/te%27st.docx&format=pdf'
+
+        metadata = ProviderMetadata('te\'st', '.docx', 'text/plain', '1234', export_url)
+        renderer = PdfRenderer(metadata, '/tmp/te\'st.docx', export_url, assets_url,
+                               'http://mfr.osf.io/export?url=http://osf.io/file/te\'st.docx')
+
+        body = renderer.render()
 
         assert '<base href="{}/{}/web/" target="_blank">'.format(assets_url, 'pdf') in body
         assert '<div id="viewer" class="pdfViewer"></div>' in body
-        assert 'DEFAULT_URL = \'{}\''.format(exported_url.url) not in body
-        assert 'DEFAULT_URL = \'{}\''.format(safe_exported_url) in body
+        assert 'DEFAULT_URL = \'{}\''.format(export_url) not in body
+        assert 'DEFAULT_URL = \'{}\''.format(safe_url) in body

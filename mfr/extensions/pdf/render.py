e8e55d5d7a9c43a224af38c15bbc51b577e86453
Fitz Elliott <fitz@cos.io>
2018-06-26 17:58:04 -0400

    pdf: turn on export support for pdfs
     * Update setup.py extension mapping to include pdfs => PdfExporter
    
     * Remove scaling factor from pdf export url, but add a custom string
       to bypass the identity exporter.  When MFR receives a request to
       export a file as the same type, it will return the unmodified file.
       Change the pdf export url's `format` query parameter to
       `export.pdf` to avoid this.

--- mfr/extensions/pdf/render.py
+++ mfr/extensions/pdf/render.py
@@ -33,11 +33,11 @@ class PdfRenderer(extension.BaseRenderer):
 
         logger.debug('Extension found in supported list!')
         exported_url = furl.furl(self.export_url)
-        if settings.EXPORT_MAXIMUM_SIZE:
+        if self.metadata.ext.lower() in settings.EXPORT_NEEDS_SCALING:
             exported_url.args['format'] = '{}.{}'.format(settings.EXPORT_MAXIMUM_SIZE,
                                                          settings.EXPORT_TYPE)
         else:
-            exported_url.args['format'] = settings.EXPORT_TYPE
+            exported_url.args['format'] = 'export.{}'.format(settings.EXPORT_TYPE)
 
         self.metrics.add('needs_export', True)
         return self.TEMPLATE.render(

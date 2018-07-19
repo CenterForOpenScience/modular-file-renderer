5b24d616fd32d5a4b8354a4cd301cc1c00370d84
Fitz Elliott <fitz@cos.io>
2018-07-19 10:19:22 -0400

    change hypothesis support toggle to default to on
     * This is safe to turn on by default; it only adds a few extra lines
       of code to the pdf render template.  The hypothes.is sidebar still
       has to be manually started by the parent frame.

--- mfr/extensions/pdf/settings.py
+++ mfr/extensions/pdf/settings.py
@@ -7,7 +7,7 @@ EXPORT_TYPE = config.get('EXPORT_TYPE', 'pdf')
 assert EXPORT_TYPE  # mandatory config
 EXPORT_MAXIMUM_SIZE = config.get('EXPORT_MAXIMUM_SIZE', '1200x1200')
 
-ENABLE_HYPOTHESIS = config.get_bool('ENABLE_HYPOTHESIS', False)
+ENABLE_HYPOTHESIS = config.get_bool('ENABLE_HYPOTHESIS', True)
 
 # supports multiple files in the form of a space separated string
 EXPORT_SUPPORTED = config.get('EXPORT_SUPPORTED', '.tiff .tif').split(' ')

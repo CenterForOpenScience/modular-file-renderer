c59443230d19150f2817d46cf94b07c848e50cf4
Joris Goosen <Joris@JorisGoosen.nl>
2023-09-21 14:00:51 +0200

    Remove superfluous checks from jasp code, just check if there is an "index.html" and show it

--- mfr/extensions/jasp/render.py
+++ mfr/extensions/jasp/render.py
@@ -10,9 +10,6 @@ from .html_processor import HTMLProcessor
 
 class JASPRenderer(extension.BaseRenderer):
 
-    # Minimum data archive version supported
-    MINIMUM_VERSION = LooseVersion('1.0.2')
-
     TEMPLATE = TemplateLookup(
         directories=[
             os.path.join(os.path.dirname(__file__), 'templates')
@@ -23,7 +20,6 @@ class JASPRenderer(extension.BaseRenderer):
     def render(self):
         try:
             with ZipFile(self.file_path) as zip_file:
-                self._check_file(zip_file)
                 body = self._render_html(zip_file, self.metadata.ext)
                 return self.TEMPLATE.render(base=self.assets_url, body=body)
         except BadZipFile as err:
@@ -52,73 +48,10 @@ class JASPRenderer(extension.BaseRenderer):
                 '{} Missing index.html.'.format(self.MESSAGE_FILE_CORRUPT),
                 extension=self.metadata.ext,
                 corruption_type='key_error',
-                reason='zip missing ./index.html',
+                reason='jasp missing ./index.html',
             )
 
         processor = HTMLProcessor(zip_file)
         processor.feed(index)
 
         return processor.final_html()
-
-    def _check_file(self, zip_file):
-        """Check if the file is OK (not corrupt)
-        :param zip_file: an opened ZipFile representing the JASP file
-        :return: True
-        """
-        # Extract manifest file content
-        try:
-            with zip_file.open('META-INF/MANIFEST.MF') as manifest_data:
-                manifest = manifest_data.read().decode('utf-8')
-        except KeyError:
-            raise exceptions.JaspFileCorruptError(
-                '{} Missing META-INF/MANIFEST.MF'.format(self.MESSAGE_FILE_CORRUPT),
-                extension=self.metadata.ext,
-                corruption_type='key_error',
-                reason='zip missing ./META-INF/MANIFEST.MF',
-            )
-
-        lines = manifest.split('\n')
-
-        # Search for Data-Archive-Version
-        dataArchiveVersionStr, createdBy = None, ''
-        for line in lines:
-            keyValue = line.split(':')
-            if len(keyValue) == 2:
-                key = keyValue[0].strip()
-                value = keyValue[1].strip()
-                if key == 'Data-Archive-Version':
-                    dataArchiveVersionStr = value
-                elif key == 'Created-By':
-                    createdBy = str(value)
-        if not dataArchiveVersionStr:
-            raise exceptions.JaspFileCorruptError(
-                '{} Data-Archive-Version not found.'.format(self.MESSAGE_FILE_CORRUPT),
-                extension=self.metadata.ext,
-                corruption_type='manifest_parse_error',
-                reason='Data-Archive-Version not found.',
-            )
-
-        # Check that the file is new enough (contains preview content)
-        dataArchiveVersion = LooseVersion(dataArchiveVersionStr)
-        try:
-            if dataArchiveVersion < self.MINIMUM_VERSION:
-                minimum_version = self.MINIMUM_VERSION.vstring
-                data_archive_version = dataArchiveVersion.vstring
-                raise exceptions.JaspVersionError(
-                    'This JASP file was created with an older data archive '
-                    'version ({}) and cannot be previewed. Minimum data archive '
-                    'version is {}.'.format(data_archive_version, minimum_version),
-                    extension=self.metadata.ext,
-                    created_by=createdBy,
-                    actual_version=data_archive_version,
-                    required_version=minimum_version,
-                )
-        except TypeError:
-            raise exceptions.JaspFileCorruptError(
-                '{} Data-Archive-Version not parsable.'.format(self.MESSAGE_FILE_CORRUPT),
-                extension=self.metadata.ext,
-                corruption_type='manifest_parse_error',
-                reason='Data-Archive-Version ({}) not parsable.'.format(dataArchiveVersionStr),
-            )
-
-        return True
